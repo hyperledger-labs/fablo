ba0e7376b374bb731f1a86231c7b0b568a306aa5
Piotr Jasiak <piotr.jasiak@softwaremill.com>
2019-11-29 16:01:47 +0100

    template for docker-compose

--- generators/create/index.js
+++ generators/create/index.js
@@ -5,18 +5,18 @@ const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
 
   async prompting() {
-    // const questions = [{
-    //   type: 'list',
-    //   name: 'subgenerator',
-    //   message: 'Select asset to create',
-    //   choices: [
-    //     {name: 'Network', value: 'create-network'},
-    //     {name: 'Organisation', value: 'create-organisation'},
-    //     {name: 'Peer', value: 'create-peer'}
-    //   ]
-    // }];
-    // const answers = await this.prompt(questions);
-    // Object.assign(this.options, answers);
+      // const questions = [{
+      //     type: 'list',
+      //     name: 'subgenerator',
+      //     message: 'Select asset to create',
+      //     choices: [
+      //         {name: 'Network', value: 'create-network'},
+      //         {name: 'Organisation', value: 'create-org'},
+      //         {name: 'Peer', value: 'create-peer'}
+      //     ]
+      // }];
+      // const answers = await this.prompt(questions);
+      // Object.assign(this.options, answers);
 
   }
 
