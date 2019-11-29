b7c25680e3bfa5273603e769affcab166b05ed34
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2019-11-29 20:44:36 +0100

    store an array of orgs
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/create/index.js
+++ generators/create/index.js
@@ -1,24 +1,23 @@
 'use strict';
 
 const Generator = require('yeoman-generator');
+const utils = require('../utils');
 
 module.exports = class extends Generator {
 
   async configuring() {
-    await this.config.save(); // TODO how to remove the initial file??
-
     const OrgGenerator = require.resolve('../create-org');
     const CAGenerator = require.resolve('../create-ca');
     const OrdererGenerator = require.resolve('../create-orderer');
     const PeerGenerator = require.resolve('../create-peer');
 
     this
-      .composeWith(OrgGenerator, {orgNamespace: 'root'})
-      .composeWith(CAGenerator, {orgNamespace: 'root'})
-      .composeWith(OrdererGenerator, {orgNamespace: 'root'})
-      .composeWith(OrgGenerator, {orgNamespace: 'org1'})
-      .composeWith(CAGenerator, {orgNamespace: 'org1'})
-      .composeWith(PeerGenerator, {orgNamespace: 'org1'});
+      .composeWith(OrgGenerator, {orgKey: utils.rootKey})
+      .composeWith(CAGenerator, {orgKey: utils.rootKey})
+      .composeWith(OrdererGenerator, {orgKey: utils.rootKey})
+      .composeWith(OrgGenerator, {orgKey: 'org1'})
+      .composeWith(CAGenerator, {orgKey: 'org1'})
+      .composeWith(PeerGenerator, {orgKey: 'org1'});
   }
 
 };
