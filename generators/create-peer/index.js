27f35046656a2239ccf54afe08ab95863fe379c9
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2019-11-29 20:44:36 +0100

    store an array of orgs

--- generators/create-peer/index.js
+++ generators/create-peer/index.js
@@ -5,25 +5,27 @@ const utils = require('../utils');
 
 const defaultPeerPrefix = 'peer';
 const defaultNumberOfInstances = 3;
+const configKey = 'peer';
 
 module.exports = class extends Generator {
 
   async prompting() {
-    const orgNamespace = this.options.orgNamespace;
+    const orgKey = this.options.orgKey;
+    const {prefix, instances} = await utils.loadConfig(this.config, orgKey, configKey);
 
     const questions = [{
       type: 'input',
       name: 'prefix',
-      message: `[${orgNamespace}] Peer:\n${utils.tab}hostname prefix`,
-      default: defaultPeerPrefix,
+      message: `[${orgKey}] Peer:\n${utils.tab}hostname prefix`,
+      default: prefix || defaultPeerPrefix,
     }, {
       type: 'number',
       name: 'instances',
       message: `${utils.tab}number of instances`,
-      default: defaultNumberOfInstances,
+      default: instances || defaultNumberOfInstances,
     }];
     const answers = await this.prompt(questions);
-    await utils.updateNamespace(this.config, orgNamespace, 'peer', answers);
+    await utils.saveConfig(this.config, orgKey, configKey, answers);
   }
 
 };
