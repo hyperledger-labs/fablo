c1c8f9b7a175ba56d1d018cf2e906a38800f55e9
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2019-11-29 15:03:04 +0100

    first approach to org/ca/ord/peer generator

--- generators/export/index.js
+++ generators/export/index.js
@@ -7,12 +7,12 @@
 const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
 
-    desc() {
-        this.log('Exports produced config to config.json, which can be shared between organizations or developers');
-    }
+  desc() {
+    this.log('Exports produced config to config.json, which can be shared between organizations or developers');
+  }
 
-    splash() {
-        this.log('Nothing there yet... Sorry !');
-    }
+  splash() {
+    this.log('Nothing there yet... Sorry !');
+  }
 
 };
