f65cb43971ace88173a70dee09e0e6c6db67504c
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2019-11-29 22:33:08 +0100

    Setup - Extracted compose to separate setup

--- generators/setup/index.js
+++ generators/setup/index.js
@@ -10,34 +10,21 @@ module.exports = class extends Generator {
     async prompting() {
         const questions = [{
             type: 'list',
-            name: 'env-type',
+            name: 'setupType',
             message: 'Select type of environment to create',
             choices: [
-                {name: 'docker-compose', value: 'docker-compose'},
-                {name: 'Helm', value: 'helm'}
+                {name: 'docker-compose', value: 'setup-compose'},
+                {name: 'Helm', value: 'setup-helm'}
             ]
         }];
         const answers = await this.prompt(questions);
         Object.assign(this.options, answers);
     }
 
-    async writing() {
-        const rootGenerator = await this.config.get('root');
-        this.fs.copyTpl(
-            this.templatePath('network/docker-compose.yaml'),
-            this.destinationPath('network-compose/docker-compose.yaml'),
-            {
-                "rootDomain": rootGenerator.organization.domain,
-                "rootName": rootGenerator.organization.name
-            }
-        );
-        this.fs.copyTpl(
-            this.templatePath('network/.env'),
-            this.destinationPath('network-compose/.env'),
-            {
-                "rootDomain": rootGenerator.organization.domain,
-                "rootName": rootGenerator.organization.name
-            }
-        );
+    async configuring() {
+        const {setup} = this.options;
+        this.log(`This generator can also be run with: yo fabric-network:${setup.setupType}`);
+        this.composeWith(require.resolve(`../${setup.setupType}`), this.options);
     }
+
 };
