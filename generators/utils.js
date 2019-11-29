abd9d7692700ad76762406c0b11f55483b3c0a6d
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2019-11-29 16:29:28 +0100

    better namespaces
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/utils.js
+++ generators/utils.js
@@ -1,13 +1,8 @@
 module.exports = {
   updateNamespace,
-  getNamespace,
 };
 
-function getNamespace(options) {
-  return !!(options || {}).isRoot ? 'root' : 'orgs';
-}
-
-async function updateNamespace(config, namespace, key, value) {
-  const current = await config.get(namespace);
-  return await config.set(namespace, {...current, [key]: value});
+async function updateNamespace(config, orgNamespace, key, value) {
+  const current = await config.get(orgNamespace);
+  return await config.set(orgNamespace, {...current, [key]: value});
 }
