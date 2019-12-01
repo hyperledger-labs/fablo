fdcd28cb64acb344d35c589ccf42712b59f8d659
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2019-12-01 11:43:37 +0100

    Compose - Added appending organizations to docker-compose

--- generators/setup-compose/index.js
+++ generators/setup-compose/index.js
@@ -8,13 +8,23 @@ const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
 
     async writing() {
-        const rootGenerator = await this.config.get('root');
+        const rootOrganization = await this.config.get('root');
         this.fs.copyTpl(
-            this.templatePath('network/docker-compose.yaml'),
+            this.templatePath('network/docker-compose-base.yaml'),
             this.destinationPath('network-compose/docker-compose.yaml'),
             {
-                "rootDomain": rootGenerator.organization.domain,
-                "rootName": rootGenerator.organization.name
+                root: {
+                    name: rootOrganization.organization.name,
+                    domain: rootOrganization.organization.domain,
+                    ca: {
+                        name: rootOrganization.ca.prefix
+                    },
+                    orderer: {
+                        name: rootOrganization.orderer.prefix,
+                    }
+                },
+
+                "orgs": ["gpw", "knf", "zaiks"]
             }
         );
         this.fs.copyTpl(
