14389563404451c22596ea1426e07178cee15ff3
Jakub Dzikowski <jakub@bezspacji.pl>
2020-02-21 16:01:52 +0100

    first snapshot test for ca

--- generators/create-ca/index.test.js
+++ generators/create-ca/index.test.js
@@ -3,14 +3,22 @@ const path = require('path');
 const fs = require('fs');
 
 describe(':create-ca', () => {
-   it('should create CA', async () => {
+  it('should create CA with defaults', async () => {
+    const dir = await helpers.run(__dirname).toPromise();
+
+    const file = fs.readFileSync(path.join(dir, '.yo-rc.json'));
+
+    expect(file.toString()).toMatchSnapshot();
+  });
+
+  it('should create CA', async () => {
     const dir = await helpers.run(__dirname)
       .withOptions({ orgKey: 'black-pearl' })
-      .withPrompts({ prefix: 'aa', generate: true })
+      .withPrompts({ prefix: 'black-org-', generate: true })
       .toPromise();
 
-    console.log(dir);
-const file =    fs.readFileSync(path.join(dir, '.yo-rc.json'));
-console.log(file)
+    const file = fs.readFileSync(path.join(dir, '.yo-rc.json'));
+
+    expect(file.toString()).toMatchSnapshot();
   });
 });
