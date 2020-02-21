6c6274ee96c445ce415d73861d9ad9180701c62b
Jakub Dzikowski <jakub@bezspacji.pl>
2020-02-21 15:29:58 +0100

    eslint config file added

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    "jest/globals": true
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+  },
+  plugins: ['jest']
+};
