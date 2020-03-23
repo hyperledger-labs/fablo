35a23f8e003403b451e39ba134bf167951a13192
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-03-23 10:06:27 +0100

    Phase 1 - working network setup without channel, cute sout
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/setup-compose2/templates/fabric-compose/scripts/base-commands.sh
+++ generators/setup-compose2/templates/fabric-compose/scripts/base-commands.sh
@@ -1,27 +1,27 @@
 function networkUp() {
-  printf "============ \U1F913 Generating configs \U1F913 ================= \n"
+  printf "============ \U1F913 Generating configs \U1F913 ====================== \n"
   certsGenerate "fabric-config" "./fabric-config/crypto-config"
   genesisBlockCreate "fabric-config" "./fabric-config/config"
 
-  printf "============ \U1F680 Starting network \U1F680 ================== \n"
+  printf "============ \U1F680 Starting network \U1F680 ======================= \n"
   cd fabric-compose
   docker-compose up -d
   cd ..
 
-  printf "============ \U1F984 Done! Enjoy your fresh network \U1F984 ==== \n"
+  printf "============ \U1F984 Done! Enjoy your fresh network \U1F984 ========= \n"
 }
 
 function networkDown() {
-  printf "============ \U1F916 Stopping network \U1F916 ================== \n"
+  printf "============ \U1F916 Stopping network \U1F916 ======================= \n"
   cd fabric-compose
   docker-compose down
   cd ..
 
-  printf "\nRemoving generates configs ... \U1F5D1 \n"
+  printf "\nRemoving generated configs ... \U1F5D1 \n"
   rm -rf fabric-config/config
   rm -rf fabric-config/crypto-config
 
-  printf "============ \U1F5D1 Done! Network was purged \U1F5D1 ============= \n"
+  printf "============ \U1F5D1 Done! Network was purged \U1F5D1 ================== \n"
 }
 
 function networkRerun() {
