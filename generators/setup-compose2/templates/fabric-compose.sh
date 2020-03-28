0cd28064639cfa4960af042973184e5226641017
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-03-28 20:41:08 +0100

    Phase 3 - Added scripts for chaincode
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/setup-compose2/templates/fabric-compose.sh
+++ generators/setup-compose2/templates/fabric-compose.sh
@@ -13,12 +13,15 @@ elif [ "$1" = "down" ]; then
   networkDown
 elif [ "$1" = "rerun" ]; then
   networkRerun
+elif [ "$1" = "chaincodes" ] && [ "$2" = "install" ]; then
+  installChaincodes
 elif [ "$1" = "help" ]; then
   printHelp
 elif [ "$1" = "--help" ]; then
   printHelp
 else
   echo "No command specified"
-  echo "Allowed options: up, down, start, stop, rerun"
-  echo "Use help or --help for more information"
+  echo "Basic commands are: up, down, start, stop, rerun"
+  echo "Also check: 'chaincodes install'"
+  echo "Use 'help' or '--help' for more information"
 fi
