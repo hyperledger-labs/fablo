d872858058975fc46dab192802bc3f66d165cbe6
Jakub Dzikowski <jakub@bezspacji.pl>
2020-04-28 15:39:46 +0200

    test samples
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

---
+++ e2e/generated.test.js
@@ -0,0 +1,34 @@
+const { execSync } = require('child_process');
+
+const executeCommand = (c) => execSync(c, { encoding: 'utf-8' });
+
+const dir01 = 'e2e/__tmp__/sample-01';
+const dir02 = 'e2e/__tmp__/sample-02';
+
+const getFiles = (dir) => executeCommand(`find ${dir}/* -type f | sort`)
+  .split('\n')
+  .filter((s) => !!s.length);
+
+const testFilesExistence = (files) => {
+  it('should create proper files', () => {
+    expect(files).toMatchSnapshot();
+  });
+};
+
+const testFilesContent = (files) => files.forEach((f) => {
+  it(`should create proper ${f}`, () => {
+    expect(executeCommand(`cat ${f}`)).toMatchSnapshot();
+  });
+});
+
+describe(dir01, () => {
+  const files = getFiles(dir01);
+  testFilesExistence(files);
+  testFilesContent(files);
+});
+
+describe(dir02, () => {
+  const files = getFiles(dir02);
+  testFilesExistence(files);
+  testFilesContent(files);
+});
