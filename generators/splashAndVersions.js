aac0bf919153727b150029d7ae3f8e6b382540dd
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-05-02 12:07:09 +0200

    removed old generators - lint fixes
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/splashAndVersions.js
+++ generators/splashAndVersions.js
@@ -4,18 +4,18 @@ const supportedFabricVersions = ['1.4.3', '1.4.4'];
 const supportedFabrikkaVersions = ['alpha-0.0.1'];
 
 function splashScreen() {
-    return 'Fabrikka is powered by :\n'
+  return `${'Fabrikka is powered by :\n'
         + ' _____        __ _                         ___  ____ _ _ \n'
         + '/  ___|      / _| |                        |  \\/  (_) | |\n'
         + '\\ `--.  ___ | |_| |___      ____ _ _ __ ___| .  . |_| | |\n'
         + ' `--. \\/ _ \\|  _| __\\ \\ /\\ / / _` | \'__/ _ \\ |\\/| | | | |\n'
         + '/\\__/ / (_) | | | |_ \\ V  V / (_| | | |  __/ |  | | | | |\n'
         + '\\____/ \\___/|_|  \\__| \\_/\\_/ \\__,_|_|  \\___\\_|  |_/_|_|_|\n'
-        + '=========================================================== v: '+fabrikkaVersion;
+        + '=========================================================== v: '}${fabrikkaVersion}`;
 }
 
 module.exports = {
-    splashScreen,
-    supportedFabricVersions,
-    supportedFabrikkaVersions
+  splashScreen,
+  supportedFabricVersions,
+  supportedFabrikkaVersions,
 };
