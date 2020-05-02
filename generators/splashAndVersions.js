cf6584e5a5fe3a75dfa2878c314c17ec773d99d7
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-05-02 11:44:06 +0200

    removed old generators
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

---
+++ generators/splashAndVersions.js
@@ -0,0 +1,21 @@
+const fabrikkaVersion = ['alpha-0.0.1'];
+
+const supportedFabricVersions = ['1.4.3', '1.4.4'];
+const supportedFabrikkaVersions = ['alpha-0.0.1'];
+
+function splashScreen() {
+    return 'Fabrikka is powered by :\n'
+        + ' _____        __ _                         ___  ____ _ _ \n'
+        + '/  ___|      / _| |                        |  \\/  (_) | |\n'
+        + '\\ `--.  ___ | |_| |___      ____ _ _ __ ___| .  . |_| | |\n'
+        + ' `--. \\/ _ \\|  _| __\\ \\ /\\ / / _` | \'__/ _ \\ |\\/| | | | |\n'
+        + '/\\__/ / (_) | | | |_ \\ V  V / (_| | | |  __/ |  | | | | |\n'
+        + '\\____/ \\___/|_|  \\__| \\_/\\_/ \\__,_|_|  \\___\\_|  |_/_|_|_|\n'
+        + '=========================================================== v: '+fabrikkaVersion;
+}
+
+module.exports = {
+    splashScreen,
+    supportedFabricVersions,
+    supportedFabrikkaVersions
+};
