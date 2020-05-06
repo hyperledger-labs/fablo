d6437c63a0447f3ebeaf87660de37307a6ccc0d7
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-05-06 11:18:16 +0200

    Chaincode TLS improvements - better idents in generated script
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/setup-compose/templates/fabric-compose/scripts/commands-generated.sh
+++ generators/setup-compose/templates/fabric-compose/scripts/commands-generated.sh
@@ -4,11 +4,20 @@ function installChaincodes() {
        org.peers.forEach(function (peer) {
   %>
   printf "============ \U1F60E Installing '<%= chaincode.name %>' on <%= chaincode.channel.name %>/<%= org.name %>/<%= peer.name %> \U1F60E ============== \n"
+  <% if(!networkSettings.tls) { -%>
   chaincodeInstall "<%= chaincode.directory %>" "<%= chaincode.name %>" "<%= chaincode.version %>" "java" "<%= chaincode.channel.name %>" "<%= peer.address %>:7051" "<%= rootOrg.ordererHead.address %>:7050" "cli.<%= org.domain %>" # TODO to mi sie nie podoba. a gdzie uprawnienia ?
+  <% } else { -%>
+  chaincodeInstallTls "<%= chaincode.directory %>" "<%= chaincode.name %>" "<%= chaincode.version %>" "java" "<%= chaincode.channel.name %>" "<%= peer.address %>:7051" "<%= rootOrg.ordererHead.address %>:7050" "cli.<%= org.domain %>" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+  <% } -%>
 
   printf "==== \U1F618 Instantiating '<%= chaincode.name %>' on <%= chaincode.channel.name %>/<%= org.name %>/<%= peer.name %> \U1F618 ==== \n"
-  chaincodeInstantiate "<%= chaincode.directory %>" "<%= chaincode.name %>" "<%= chaincode.version %>" "java" "<%= chaincode.channel.name %>" "<%= peer.address %>:7051" "<%= rootOrg.ordererHead.address %>:7050" "cli.<%= org.domain %>" "<%- chaincode.init %>" "<%- chaincode.endorsement %>"
+  <% if(!networkSettings.tls) { -%>
+  chaincodeInstantiate "<%= chaincode.directory %>" "<%= chaincode.name %>" "<%= chaincode.version %>" "java" "<%= chaincode.channel.name %>" "<%= peer.address %>:7051" "<%= rootOrg.ordererHead.address %>:7050" "cli.<%= org.domain %>" '<%- chaincode.init %>' "<%- chaincode.endorsement %>"
+  <% } else { -%>
+  chaincodeInstantiateTls "<%= chaincode.directory %>" "<%= chaincode.name %>" "<%= chaincode.version %>" "java" "<%= chaincode.channel.name %>" "<%= peer.address %>:7051" "<%= rootOrg.ordererHead.address %>:7050" "cli.<%= org.domain %>" '<%- chaincode.init %>' "<%- chaincode.endorsement %>" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+  <% } -%>
   <% })})}) -%>
+
 }
 
 function networkUp() {
@@ -16,8 +25,8 @@ function networkUp() {
   printf "===== \U1F512 Generating crypto material for org <%= rootOrg.organization.name %> \U1F512 ===== \n"
   certsGenerate "fabric-config" "crypto-config-root.yaml" "ordererOrganizations/<%= rootOrg.organization.domain %>" "./fabric-config/crypto-config/"
   <% orgs.forEach(function(org){  %>
-  printf "===== \U1F512 Generating crypto material for <%= org.organization.name %> \U1F512 ===== \n"
-  certsGenerate "fabric-config" "crypto-config-<%= org.organization.name.toLowerCase() %>.yaml" "peerOrganizations/<%= org.organization.domain %>" "./fabric-config/crypto-config/"
+  printf "===== \U1F512 Generating crypto material for <%= org.name %> \U1F512 ===== \n"
+  certsGenerate "fabric-config" "<%= org.cryptoConfigFileName %>.yaml" "peerOrganizations/<%= org.domain %>" "./fabric-config/crypto-config/"
   <% }) %>
 
   printf "===== \U1F3E0 Generating genesis block \U1F3E0 ===== \n"
@@ -42,19 +51,19 @@ function networkUp() {
   <% if(orgNo==0 && peerNo==0) { -%>
   printf "============ \U1F63B Creating '<%= channel.name %>' on <%= org.name %>/<%= peer.name %> \U1F63B ================== \n"
   <% if(!networkSettings.tls) { -%>
-  docker exec -i cli.<%= org.domain %> bash -c \
+  docker exec -it cli.<%= org.domain %> bash -c \
     "source scripts/channel_fns.sh; createChannelAndJoin '<%= channel.name %>' '<%= org.mspName %>' '<%= peer.address %>:7051' 'crypto/users/Admin@<%= org.domain %>/msp' '<%= rootOrg.ordererHead.address %>:7050';"
   <% } else { -%>
-  docker exec -i cli.<%= org.domain %> bash -c \
+  docker exec -it cli.<%= org.domain %> bash -c \
     "source scripts/channel_fns.sh; createChannelAndJoinTls '<%= channel.name %>' '<%= org.mspName %>' '<%= peer.address %>:7051' 'crypto/users/Admin@<%= org.domain %>/msp' 'crypto/users/Admin@<%= org.domain %>/tls' 'crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem' '<%= rootOrg.ordererHead.address %>:7050';"
   <% } %>
   <% } else { -%>
   printf "====== \U1F638 Joining '<%= channel.name %>' on  <%= org.name %>/<%= peer.name %> \U1F638 ====== \n"
   <% if(!networkSettings.tls) { -%>
-  docker exec -i cli.<%= org.domain %> bash -c \
+  docker exec -it cli.<%= org.domain %> bash -c \
     "source scripts/channel_fns.sh; fetchChannelAndJoin '<%= channel.name %>' '<%= org.mspName %>' '<%= peer.address %>:7051' 'crypto/users/Admin@<%= org.domain %>/msp' '<%= rootOrg.ordererHead.address %>:7050';"
   <% } else { -%>
-  docker exec -i cli.<%= org.domain %> bash -c \
+  docker exec -it cli.<%= org.domain %> bash -c \
     "source scripts/channel_fns.sh; fetchChannelAndJoinTls '<%= channel.name %>' '<%= org.mspName %>' '<%= peer.address %>:7051' 'crypto/users/Admin@<%= org.domain %>/msp' 'crypto/users/Admin@<%= org.domain %>/tls' 'crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem' '<%= rootOrg.ordererHead.address %>:7050';"
   <% } %>
   <% } -%>
