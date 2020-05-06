6bcdbc3fc248b494963092319ad7c2e2578dd29b
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-05-06 11:35:07 +0200

    Chaincode TLS improvements - added tests for TLS
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- e2e-generate/entrypoint.sh
+++ e2e-generate/entrypoint.sh
@@ -21,4 +21,6 @@ sudo npm link &&
   sudo mkdir -p "$basedir" &&
   sudo chown -R yeoman:yeoman "$basedir" &&
   generate "sample-01" "fabrikkaConfig-1org-1channel-1chaincode.json" &&
-  generate "sample-02" "fabrikkaConfig-2orgs-2channels-1chaincode.json"
+  generate "sample-02" "fabrikkaConfig-2orgs-2channels-1chaincode.json" &&
+  generate "sample-03" "fabrikkaConfig-1org-1channel-1chaincode-tls.json" &&
+  generate "sample-04" "fabrikkaConfig-2orgs-2channels-1chaincode-tls.json"
