c47db2700b75c5f7b25fd9669549e06f7740dcce
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-08-25 14:51:54 +0200

    Raft - Enriched supported Fabric versions
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/config.js
+++ generators/config.js
@@ -1,6 +1,6 @@
 const fabrikkaVersion = ['alpha-0.0.1'];
 
-const supportedFabricVersions = ['1.4.3', '1.4.4'];
+const supportedFabricVersions = ['1.3.0', '1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.4.4', '1.4.5', '1.4.6', '1.4.7', '1.4.8'];
 const supportedFabrikkaVersions = ['alpha-0.0.1'];
 
 function splashScreen() {
