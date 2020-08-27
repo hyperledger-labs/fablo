9b2fe423f940bb782416b7093a3cb521c1a6f019
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-08-27 09:08:45 +0200

    Raft - Added more tests
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- e2e/generated.test.js
+++ e2e/generated.test.js
@@ -4,11 +4,15 @@ const executeCommand = (c) => execSync(c, { encoding: 'utf-8' });
 
 const dir01 = 'e2e/__tmp__/sample-01';
 const dir02 = 'e2e/__tmp__/sample-02';
+const dir03 = 'e2e/__tmp__/sample-03';
+const dir04 = 'e2e/__tmp__/sample-04';
+const dir05 = 'e2e/__tmp__/sample-05';
+const dir06 = 'e2e/__tmp__/sample-06';
 
 const getFiles = (dir) => executeCommand(`find ${dir}/* -type f`)
-    .split('\n')
-    .sort()
-    .filter((s) => !!s.length);
+  .split('\n')
+  .filter((s) => !!s.length)
+  .sort();
 
 const testFilesExistence = (files) => {
   it('should create proper files', () => {
@@ -30,6 +34,30 @@ describe(dir01, () => {
 
 describe(dir02, () => {
   const files = getFiles(dir02);
+  testFilesExistence(files);
+  testFilesContent(files);
+});
+
+describe(dir03, () => {
+  const files = getFiles(dir03);
+  testFilesExistence(files);
+  testFilesContent(files);
+});
+
+describe(dir04, () => {
+  const files = getFiles(dir04);
+  testFilesExistence(files);
+  testFilesContent(files);
+});
+
+describe(dir05, () => {
+  const files = getFiles(dir04);
+  testFilesExistence(files);
+  testFilesContent(files);
+});
+
+describe(dir06, () => {
+  const files = getFiles(dir04);
   testFilesExistence(files);
   testFilesContent(files);
 });
