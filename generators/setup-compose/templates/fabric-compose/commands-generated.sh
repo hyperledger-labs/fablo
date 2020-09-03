fad69374583be7fcb6a349d7b2fbba16c7581a98
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-09-03 23:42:32 +0200

    Bash OO removal - own readlink -f implementation
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/setup-compose/templates/fabric-compose/commands-generated.sh
+++ generators/setup-compose/templates/fabric-compose/commands-generated.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+function get_realpath() {
+  [[ ! -f "$1" ]] && return 1 # failure : file does not exist.
+  [[ -n "$no_symlinks" ]] && local pwdp='pwd -P' || local pwdp='pwd' # do symlinks.
+  echo "$( cd "$( echo "${1%/*}" )" 2>/dev/null; $pwdp )"/"${1##*/}" # echo result.
+  return 0 # success
+}
 
-SCRIPT=$(readlink -f "$0")
+SCRIPT=$(get_realpath "$0")
 BASEDIR=$(dirname "$SCRIPT")
 
 function installChaincodes() {
