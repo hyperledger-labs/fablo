cac6a46584488232e27b1ceeb74678d6f3ea3afc
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-09-03 23:42:32 +0200

    Bash OO removal - own readlink -f implementation

--- generators/setup-compose/templates/fabric-compose.sh
+++ generators/setup-compose/templates/fabric-compose.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-SCRIPT=$(readlink -f "$0")
+function get_realpath() {
+  [[ ! -f "$1" ]] && return 1 # failure : file does not exist.
+  [[ -n "$no_symlinks" ]] && local pwdp='pwd -P' || local pwdp='pwd' # do symlinks.
+  echo "$( cd "$( echo "${1%/*}" )" 2>/dev/null; $pwdp )"/"${1##*/}" # echo result.
+  return 0 # success
+}
+
+SCRIPT=$(get_realpath "$0")
 BASEDIR=$(dirname "$SCRIPT")
 
 source "$BASEDIR"/fabric-compose/scripts/base-help.sh
