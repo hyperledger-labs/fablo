6200902fa4bc4fb885a43eac3a87c45abde6aab4
Jakub Dzikowski <jakub@bezspacji.pl>
2020-10-02 05:24:37 +0200

    created explicit network tests for simple network
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

---
+++ e2e-network/test-01-simple.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+TEST_TMP="$0-tmp"
+FABRIKKA_HOME="$TEST_TMP/../../"
+mkdir -p "$TEST_TMP"
+
+CONFIG="$FABRIKKA_HOME/samples/fabrikkaConfig-1org-1channel-1chaincode.json"
+
+networkUpAsync() {
+  (sh "$FABRIKKA_HOME/docker-generate.sh" "$CONFIG" "$TEST_TMP" &&
+    cd "$TEST_TMP" &&
+    (sh fabric-compose.sh up &))
+}
+
+networkDown() {
+  (cd "$TEST_TMP" && sh fabric-compose.sh down)
+}
+
+waitFor() {
+  sh "$TEST_TMP/../wait-for-container.sh" "$1" "$2"
+}
+
+networkUpAsync
+
+waitFor "ca.root.com" "Listening on http://0.0.0.0:7054" &&
+  waitFor "orderer0.root.com" "Created and starting new chain my-channel1" &&
+  waitFor "ca.org1.com" "Listening on http://0.0.0.0:7054" &&
+  waitFor "peer0.org1.com" "Elected as a leader, starting delivery service for channel my-channel1" &&
+  waitFor "peer1.org1.com" "Elected as a leader, starting delivery service for channel my-channel1" &&
+  networkDown
