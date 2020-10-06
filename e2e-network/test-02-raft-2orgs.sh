8536e50eb26f6f6aa30801ddc089e55b3e273dcc
Jakub Dzikowski <jakub@bezspacji.pl>
2020-10-06 15:15:34 +0200

    update test to new version
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- e2e-network/test-02-raft-2orgs.sh
+++ e2e-network/test-02-raft-2orgs.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-TEST_TMP="$0.tmpdir"
-FABRIKKA_HOME="$TEST_TMP/../../"
-mkdir -p "$TEST_TMP"
+TEST_TMP="$(mkdir -p "$0.tmpdir" && (cd "$0.tmpdir" && pwd))"
+FABRIKKA_HOME="$TEST_TMP/../.."
 
 CONFIG="$FABRIKKA_HOME/samples/fabrikkaConfig-2orgs-2channels-1chaincode-tls-raft.json"
+CHAINCODE="$FABRIKKA_HOME/samples/chaincode-kv-node"
 
 networkUpAsync() {
-  (sh "$FABRIKKA_HOME/docker-generate.sh" "$CONFIG" "$TEST_TMP" &&
+  (rm -rf "$TEST_TMP" &&
+    mkdir -p "$TEST_TMP" &&
+    sh "$FABRIKKA_HOME/docker-generate.sh" "$CONFIG" "$TEST_TMP" &&
     cd "$TEST_TMP" &&
+    cp -R "$CHAINCODE" "$TEST_TMP" &&
     (sh fabrikka.sh up &))
 }
 
@@ -16,23 +19,27 @@ networkDown() {
   (cd "$TEST_TMP" && sh fabrikka.sh down)
 }
 
-waitFor() {
+waitForContainer() {
   sh "$TEST_TMP/../wait-for-container.sh" "$1" "$2"
+}
+
+waitForChaincode() {
+  sh "$TEST_TMP/../wait-for-chaincode.sh" "$1" "$2" "$3" "$4"
 }
 
 networkUpAsync
 
-waitFor "ca.root.com" "Listening on http://0.0.0.0:7054" &&
-  waitFor "orderer0.root.com" "Starting Raft node channel=my-channel1" &&
-  waitFor "orderer0.root.com" "Starting Raft node channel=my-channel2" &&
-  waitFor "orderer1.root.com" "Starting Raft node channel=my-channel1" &&
-  waitFor "orderer1.root.com" "Starting Raft node channel=my-channel2" &&
-  waitFor "orderer2.root.com" "Starting Raft node channel=my-channel1" &&
-  waitFor "orderer2.root.com" "Starting Raft node channel=my-channel2" &&
-  waitFor "ca.org1.com" "Listening on http://0.0.0.0:7054" &&
-  waitFor "peer0.org1.com" "Elected as a leader, starting delivery service for channel my-channel1" &&
-  waitFor "peer1.org1.com" "Elected as a leader, starting delivery service for channel my-channel2" &&
-  waitFor "ca.org2.com" "Listening on http://0.0.0.0:7054" &&
-  waitFor "peer0.org2.com" "Elected as a leader, starting delivery service for channel my-channel1" &&
-  waitFor "peer1.org2.com" "Elected as a leader, starting delivery service for channel my-channel2" &&
+waitForContainer "ca.root.com" "Listening on http://0.0.0.0:7054" &&
+  waitForContainer "orderer0.root.com" "Starting Raft node channel=my-channel1" &&
+  waitForContainer "orderer0.root.com" "Starting Raft node channel=my-channel2" &&
+  waitForContainer "orderer1.root.com" "Starting Raft node channel=my-channel1" &&
+  waitForContainer "orderer1.root.com" "Starting Raft node channel=my-channel2" &&
+  waitForContainer "orderer2.root.com" "Starting Raft node channel=my-channel1" &&
+  waitForContainer "orderer2.root.com" "Starting Raft node channel=my-channel2" &&
+  waitForContainer "ca.org1.com" "Listening on http://0.0.0.0:7054" &&
+  waitForContainer "peer0.org1.com" "Elected as a leader, starting delivery service for channel my-channel1" &&
+  waitForContainer "peer1.org1.com" "Elected as a leader, starting delivery service for channel my-channel2" &&
+  waitForContainer "ca.org2.com" "Listening on http://0.0.0.0:7054" &&
+  waitForContainer "peer0.org2.com" "Elected as a leader, starting delivery service for channel my-channel1" &&
+  waitForContainer "peer1.org2.com" "Elected as a leader, starting delivery service for channel my-channel2" &&
   networkDown
