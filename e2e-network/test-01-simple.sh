5cc9268fe1056b5a087905d3fcb5dda43ce2c036
Jakub Dzikowski <jakub@bezspacji.pl>
2020-10-06 06:43:59 +0200

    down network after test, wait less time, paths fixes
    Signed-off-by: Jakub Dzikowski <jakub.dzikowski@softwaremill.com>

--- e2e-network/test-01-simple.sh
+++ e2e-network/test-01-simple.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-TEST_TMP="$0.tmpdir"
-FABRIKKA_HOME="$TEST_TMP/../../"
+TEST_TMP="$(mkdir -p "$0.tmpdir" && (cd "$0.tmpdir" && pwd))"
+FABRIKKA_HOME="$TEST_TMP/../.."
 
 CONFIG="$FABRIKKA_HOME/samples/fabrikkaConfig-1org-1channel-1chaincode.json"
 CHAINCODE="$FABRIKKA_HOME/samples/chaincode-kv-node"
@@ -16,8 +16,7 @@ networkUpAsync() {
 }
 
 networkDown() {
-  exit 1
-  #  (cd "$TEST_TMP" && sh fabrikka.sh down)
+  (cd "$TEST_TMP" && sh fabrikka.sh down)
 }
 
 waitForContainer() {
@@ -35,5 +34,5 @@ waitForContainer "ca.root.com" "Listening on http://0.0.0.0:7054" &&
   waitForContainer "ca.org1.com" "Listening on http://0.0.0.0:7054" &&
   waitForContainer "peer0.org1.com" "Elected as a leader, starting delivery service for channel my-channel1" &&
   waitForContainer "peer1.org1.com" "Elected as a leader, starting delivery service for channel my-channel1" &&
-  waitForChaincode "chaincode1" "0.0.1"
-networkDown
+  waitForChaincode "chaincode1" "0.0.1" &&
+  networkDown
