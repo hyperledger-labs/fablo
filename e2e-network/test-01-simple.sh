dafce4ad5159580a4b1500b0375e6172d7617250
Jakub Dzikowski <jakub@bezspacji.pl>
2020-10-13 19:39:51 +0200

    move expectInvoke to dedicated file expect-invoke.sh
    Signed-off-by: Jakub Dzikowski <jakub.dzikowski@softwaremill.com>

--- e2e-network/test-01-simple.sh
+++ e2e-network/test-01-simple.sh
@@ -40,31 +40,7 @@ waitForChaincode() {
 }
 
 expectInvoke() {
-  cli="$1"
-  peer="$2"
-  command="$3"
-  expected="$4"
-  label="Invoke $cli/$peer $command"
-
-  expectedStatus="status:200"
-
-  response="$(
-    docker exec -e CORE_PEER_ADDRESS="$peer:7051" "$cli" peer chaincode invoke \
-      -C "my-channel1" \
-      -n "chaincode1" \
-      -c "$command" \
-      --waitForEvent \
-      2>&1
-  )"
-
-  if echo "$response" | grep "$expected"; then
-    echo "[ok] $label"
-  else
-    echo "[failed] $label
-      expected: $expected
-      actual:   $response"
-    exit 1
-  fi
+  sh "$TEST_TMP/../expect-invoke.sh" "$1" "$2" "$3" "$4"
 }
 
 networkUpAsync
