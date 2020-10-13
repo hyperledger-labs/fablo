dc72b3be4ce779c874e174d6032fb7dee0357860
Jakub Dzikowski <jakub@bezspacji.pl>
2020-10-13 14:30:09 +0200

    more debug information, port added

--- e2e-network/wait-for-chaincode.sh
+++ e2e-network/wait-for-chaincode.sh
@@ -8,16 +8,16 @@ version=$5
 search_string="Name: $chaincode, Version: $version"
 
 listChaincodes() {
-  docker exec -e CORE_PEER_ADDRESS="$peer" "$cli" peer chaincode list \
+  docker exec -e CORE_PEER_ADDRESS="$peer:7051" "$cli" peer chaincode list \
     -C "$channel" \
     --instantiated
 }
 
 for i in $(seq 1 90); do
-  echo "Verifying if chaincode ($chaincode/$version) is ready on $channel/$cli ($i)..."
+  echo "Verifying if chaincode ($chaincode/$version) is ready on $channel/$cli/$peer ($i)..."
 
   if listChaincodes 2>&1 | grep "$search_string"; then
-    echo "Chaincode $chaincode/$version is ready on $channel/$cli!"
+    echo "Chaincode $chaincode/$version is ready on $channel/$cli/$peer!"
     exit 0
   else
     sleep 1
@@ -25,6 +25,6 @@ for i in $(seq 1 90); do
 done
 
 #timeout
-echo "Failed to verify chaincode $chaincode/$version on $channel/$cli"
+echo "Failed to verify chaincode $chaincode/$version on $channel/$cli/$peer"
 listChaincodes
 exit 1
