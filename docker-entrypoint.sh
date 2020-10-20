2acf5ab0540f2b9af68831017fc2c3e0ba738062
Jakub Dzikowski <jakub@bezspacji.pl>
2020-10-20 07:08:42 +0200

    fix permission issues (separate docker run for yeoman generate command)
    Signed-off-by: Jakub Dzikowski <jakub.dzikowski@softwaremill.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,38 +5,8 @@ target="/network/docker"
 chaincodes="/network/chaincodes"
 command="$COMMAND"
 
-generateConfig() {
-  echo "NETWORK_CONFIG: $NETWORK_CONFIG"
-  echo "NETWORK_TARGET: $NETWORK_TARGET"
-  echo "CHAINCODES: $CHAINCODES"
-
-  rm -rf "$target/*" &&
-    chown -R yeoman:yeoman "$target" &&
-    (
-      cd "$target" &&
-        sudo -u yeoman yo --no-insight fabrikka:setup-docker "../..$config"
-    ) &&
-    chown -R root:root "$target"
-}
-
-#
-#
-#
-
-mkdir -p "$target"
-mkdir -p "$chaincodes"
-
 if [ "$command" = "generate" ]; then
-  echo "Generating network in $NETWORK_TARGET for $NETWORK_CONFIG"
-  generateConfig
-elif [ -z "$(ls -A "$target")" ]; then
-  echo "Target directory is empty, generating network config"
-  generateConfig
+  (cd "$target" && yo --no-insight fabrikka:setup-docker "../..$config")
 else
-  echo "Skipping generation of network config. Use command 'generate' to overwrite"
-fi
-
-if [ "$command" != "generate" ]; then
-  echo "Executing Fabrikka docker command: $command"
   sh "$target/fabrikka-docker.sh" "$command"
 fi
--- fabrikka.sh
+++ fabrikka.sh
@@ -23,13 +23,50 @@ NETWORK_CONFIG="$(fullPath "$2")"
 NETWORK_TARGET="$(mkdir -p "$(dirname "$3")" && fullPath "$3")"
 CHAINCODES="$(mkdir -p "$(dirname "$4")" && fullPath "$4")"
 
-docker run -i --rm \
-  -v "$NETWORK_CONFIG":/network/config.json \
-  -v "$NETWORK_TARGET":/network/docker \
-  -v "$CHAINCODES":/network/chaincodes \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  --env COMMAND="$COMMAND" \
-  --env NETWORK_CONFIG="$NETWORK_CONFIG" \
-  --env NETWORK_TARGET="$NETWORK_TARGET" \
-  --env CHAINCODES="$CHAINCODES" \
-  fabrikka
+yeomanGenerate() {
+  echo "Generating network config"
+  echo "    NETWORK_CONFIG: $NETWORK_CONFIG"
+  echo "    NETWORK_TARGET: $NETWORK_TARGET"
+  echo "    CHAINCODES:     $CHAINCODES"
+
+  rm -rf "$NETWORK_TARGET" &&
+    mkdir -p "$NETWORK_TARGET" &&
+    docker run -i --rm \
+      -v "$NETWORK_CONFIG":/network/config.json \
+      -v "$NETWORK_TARGET":/network/docker \
+      -v "$CHAINCODES":/network/chaincodes \
+      -v /tmp:/home/yeoman \
+      --env COMMAND="generate" \
+      --env NETWORK_CONFIG="$NETWORK_CONFIG" \
+      --env NETWORK_TARGET="$NETWORK_TARGET" \
+      --env CHAINCODES="$CHAINCODES" \
+      -u "$(id -u "${USER}"):$(id -g "${USER}")" \
+      fabrikka
+}
+
+executeNetworkCommand() {
+  docker run -i --rm \
+    -v "$NETWORK_CONFIG":/network/config.json \
+    -v "$NETWORK_TARGET":/network/docker \
+    -v "$CHAINCODES":/network/chaincodes \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --env COMMAND="$COMMAND" \
+    --env NETWORK_CONFIG="$NETWORK_CONFIG" \
+    --env NETWORK_TARGET="$NETWORK_TARGET" \
+    --env CHAINCODES="$CHAINCODES" \
+    fabrikka
+}
+
+if [ "$COMMAND" = "generate" ]; then
+  yeomanGenerate
+elif [ -z "$(ls -A "$NETWORK_TARGET")" ]; then
+  echo "Network target directory is empty"
+  yeomanGenerate
+else
+  echo "Use command 'generate' to overwrite network config"
+fi
+
+if [ "$COMMAND" != "generate" ]; then
+  echo "Executing Fabrikka docker command: $COMMAND"
+  executeNetworkCommand
+fi
