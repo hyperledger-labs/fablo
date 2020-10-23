2f1bbd1c70635b53a3f506bf3d216dcd687ef10f
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-10-23 17:06:10 +0200

    Channel scripts - stared TLS work
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/setup-docker/templates/fabric-docker/scripts/base-peer-channel-functions.sh
+++ generators/setup-docker/templates/fabric-docker/scripts/base-peer-channel-functions.sh
@@ -190,3 +190,209 @@ function peerChannelFetchFirstBlockDefault() {
 
   docker exec $CLI_NAME rm -rf /tmp/hyperledger/assets/
 }
+
+#=== TLS equivalents =========================================================
+
+function peerChannelListTls() {
+  local CLI_NAME=$1
+  local PEER_ADDRESS=$2
+  local CA_CERT=$3
+
+  echo "Listing channels using $CLI_NAME using peer $PEER_ADDRESS (TLS)..."
+  inputLog "CLI_NAME: $CLI_NAME"
+  inputLog "PEER_ADDRESS: $PEER_ADDRESS"
+
+  docker exec -e CORE_PEER_ADDRESS=$PEER_ADDRESS $CLI_NAME peer channel list --tls --cafile $CA_CERT
+}
+
+function peerChannelListDefaultTls() {
+  local CLI_NAME=$1
+  local CA_CERT=$2
+
+  echo "Listing channels using $CLI_NAME using default cli's peer (TLS)..."
+  inputLog "CLI_NAME: $CLI_NAME"
+
+  docker exec $CLI_NAME peer channel list --cafile $CA_CERT
+}
+
+function peerChannelGetInfoTls() {
+  local CHANNEL_NAME=$1
+  local CLI_NAME=$2
+  local PEER_ADDRESS=$3
+  local CA_CERT=$4
+
+
+  echo "Getting info about $CHANNEL_NAME using peer $PEER_ADDRESS (TLS)..."
+  inputLog "CHANNEL_NAME: $CHANNEL_NAME"
+  inputLog "CLI_NAME: $CLI_NAME"
+  inputLog "PEER_ADDRESS: $PEER_ADDRESS"
+
+  docker exec -e CORE_PEER_ADDRESS=$PEER_ADDRESS $CLI_NAME peer channel getinfo \
+    -c $CHANNEL_NAME --tls --cafile $CA_CERT
+}
+
+function peerChannelGetInfoDefaultTls() {
+  local CHANNEL_NAME=$1
+  local CLI_NAME=$2
+  local CA_CERT=$3
+
+  echo "Getting info about $CHANNEL_NAME using default cli's peer (TLS)..."
+  inputLog "CHANNEL_NAME: $CHANNEL_NAME"
+  inputLog "CLI_NAME: $CLI_NAME"
+
+  docker exec $CLI_NAME peer channel getinfo \
+    -c $CHANNEL_NAME --tls --cafile $CA_CERT
+}
+
+function peerChannelFetchConfigTls() {
+  local CHANNEL_NAME=$1
+  local CLI_NAME=$2
+  local CONFIG_FILE_NAME=$3
+  local PEER_ADDRESS=$4
+  local CA_CERT=$5
+
+  echo "Fetching config block from $CHANNEL_NAME using peer $PEER_ADDRESS (TLS)..."
+  inputLog "CHANNEL_NAME: $CHANNEL_NAME"
+  inputLog "CLI_NAME: $CLI_NAME"
+  inputLog "CONFIG_FILE_NAME: $CONFIG_FILE_NAME"
+  inputLog "PEER_ADDRESS: $PEER_ADDRESS"
+
+  docker exec $CLI_NAME mkdir -p /tmp/hyperledger/assets/
+  docker exec \
+    -e CORE_PEER_ADDRESS=$PEER_ADDRESS \
+    $CLI_NAME peer channel fetch config /tmp/hyperledger/assets/config_block_before.pb \
+    -c $CHANNEL_NAME --tls --cafile $CA_CERT
+
+  docker exec $CLI_NAME chmod 777 /tmp/hyperledger/assets/config_block_before.pb
+  docker exec \
+    -e CORE_PEER_ADDRESS=$PEER_ADDRESS \
+    $CLI_NAME configtxlator proto_decode \
+    --input /tmp/hyperledger/assets/config_block_before.pb \
+    --type common.Block | \
+    jq .data.data[0].payload.data.config > $CONFIG_FILE_NAME
+
+  docker exec $CLI_NAME rm -rf /tmp/hyperledger/assets/
+}
+
+function peerChannelFetchConfigDefaultTls() {
+  local CHANNEL_NAME=$1
+  local CLI_NAME=$2
+  local CONFIG_FILE_NAME=$3
+  local CA_CERT=$4
+
+  echo "Fetching config block from $CHANNEL_NAME using default cli's peer (TLS)..."
+  inputLog "CHANNEL_NAME: $CHANNEL_NAME"
+  inputLog "CLI_NAME: $CLI_NAME"
+  inputLog "CONFIG_FILE_NAME: $CONFIG_FILE_NAME"
+
+  docker exec $CLI_NAME mkdir -p /tmp/hyperledger/assets/
+  docker exec \
+    $CLI_NAME peer channel fetch config /tmp/hyperledger/assets/config_block_before.pb \
+    -c $CHANNEL_NAME --tls --cafile $CA_CERT
+
+  docker exec $CLI_NAME chmod 777 /tmp/hyperledger/assets/config_block_before.pb
+  docker exec $CLI_NAME configtxlator proto_decode \
+    --input /tmp/hyperledger/assets/config_block_before.pb \
+    --type common.Block | \
+    jq .data.data[0].payload.data.config > $CONFIG_FILE_NAME
+
+  docker exec $CLI_NAME rm -rf /tmp/hyperledger/assets/
+}
+
+function peerChannelFetchLastBlockTls() {
+  local CHANNEL_NAME=$1
+  local CLI_NAME=$2
+  local BLOCK_FILE_NAME=$3
+  local PEER_ADDRESS=$4
+  local CA_CERT=$5
+
+  echo "Fetching last block from $CHANNEL_NAME using peer $PEER_ADDRESS (TLS)..."
+  inputLog "CHANNEL_NAME: $CHANNEL_NAME"
+  inputLog "CLI_NAME: $CLI_NAME"
+  inputLog "BLOCK_FILE_NAME: $BLOCK_FILE_NAME"
+  inputLog "PEER_ADDRESS: $PEER_ADDRESS"
+
+  docker exec $CLI_NAME mkdir -p /tmp/hyperledger/blocks/
+  docker exec -e CORE_PEER_ADDRESS=$PEER_ADDRESS \
+    $CLI_NAME peer channel fetch newest /tmp/hyperledger/blocks/newest.block \
+    -c $CHANNEL_NAME --tls --cafile $CA_CERT
+  docker exec -e CORE_PEER_ADDRESS=$PEER_ADDRESS \
+    $CLI_NAME configtxlator proto_decode \
+    --input /tmp/hyperledger/blocks/newest.block \
+    --type common.Block | \
+    jq .data.data[0].payload.data.config > $BLOCK_FILE_NAME
+
+  docker exec $CLI_NAME rm -rf /tmp/hyperledger/assets/
+}
+
+function peerChannelFetchLastBlockDefaultTls() {
+  local CHANNEL_NAME=$1
+  local CLI_NAME=$2
+  local BLOCK_FILE_NAME=$3
+  local CA_CERT=$4
+
+  echo "Fetching last block from $CHANNEL_NAME using default cli's peer (TLS)..."
+  inputLog "CHANNEL_NAME: $CHANNEL_NAME"
+  inputLog "CLI_NAME: $CLI_NAME"
+  inputLog "BLOCK_FILE_NAME: $BLOCK_FILE_NAME"
+
+  docker exec $CLI_NAME mkdir -p /tmp/hyperledger/blocks/
+  docker exec \
+    $CLI_NAME peer channel fetch newest /tmp/hyperledger/blocks/newest.block \
+    -c $CHANNEL_NAME --tls --cafile $CA_CERT
+  docker exec $CLI_NAME configtxlator proto_decode \
+    --input /tmp/hyperledger/blocks/newest.block \
+    --type common.Block | \
+    jq .data.data[0].payload.data.config > $BLOCK_FILE_NAME
+
+  docker exec $CLI_NAME rm -rf /tmp/hyperledger/assets/
+}
+
+function peerChannelFetchFirstBlockTls() {
+  local CHANNEL_NAME=$1
+  local CLI_NAME=$2
+  local BLOCK_FILE_NAME=$3
+  local PEER_ADDRESS=$4
+  local CA_CERT=$5
+
+  echo "Fetching first block from $CHANNEL_NAME using peer $PEER_ADDRESS (TLS)..."
+  inputLog "CHANNEL_NAME: $CHANNEL_NAME"
+  inputLog "CLI_NAME: $CLI_NAME"
+  inputLog "BLOCK_FILE_NAME: $BLOCK_FILE_NAME"
+  inputLog "PEER_ADDRESS: $PEER_ADDRESS"
+
+  docker exec $CLI_NAME mkdir -p /tmp/hyperledger/blocks/
+  docker exec -e CORE_PEER_ADDRESS=$PEER_ADDRESS \
+    $CLI_NAME peer channel fetch oldest /tmp/hyperledger/blocks/oldest.block \
+    -c $CHANNEL_NAME --tls --cafile $CA_CERT
+  docker exec -e CORE_PEER_ADDRESS=$PEER_ADDRESS \
+    $CLI_NAME configtxlator proto_decode \
+    --input /tmp/hyperledger/blocks/oldest.block \
+    --type common.Block | \
+    jq .data.data[0].payload.data.config > $BLOCK_FILE_NAME
+
+  docker exec $CLI_NAME rm -rf /tmp/hyperledger/assets/1
+}
+
+function peerChannelFetchFirstBlockDefaultTls() {
+  local CHANNEL_NAME=$1
+  local CLI_NAME=$2
+  local BLOCK_FILE_NAME=$3
+  local CA_CERT=$4
+
+  echo "Fetching first block from $CHANNEL_NAME using default cli's peer (TLS)..."
+  inputLog "CHANNEL_NAME: $CHANNEL_NAME"
+  inputLog "CLI_NAME: $CLI_NAME"
+  inputLog "BLOCK_FILE_NAME: $BLOCK_FILE_NAME"
+
+  docker exec $CLI_NAME mkdir -p /tmp/hyperledger/blocks/
+  docker exec \
+    $CLI_NAME peer channel fetch oldest /tmp/hyperledger/blocks/oldest.block \
+    -c $CHANNEL_NAME --tls --cafile $CA_CERT
+  docker exec $CLI_NAME configtxlator proto_decode \
+    --input /tmp/hyperledger/blocks/oldest.block \
+    --type common.Block | \
+    jq .data.data[0].payload.data.config > $BLOCK_FILE_NAME
+
+  docker exec $CLI_NAME rm -rf /tmp/hyperledger/assets/
+}
