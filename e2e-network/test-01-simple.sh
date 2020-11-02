fcf19851b257df57ecc211392789ac1e7275174c
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-11-02 14:57:45 +0100

    Anchor peers update - small test improvement
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- e2e-network/test-01-simple.sh
+++ e2e-network/test-01-simple.sh
@@ -53,9 +53,9 @@ waitForContainer "ca.root.com" "Listening on http://0.0.0.0:7054" &&
   waitForChaincode "cli.org1.com" "peer1.org1.com" "my-channel1" "chaincode1" "0.0.1" &&
     waitForContainer "peer0.org1.com" "Learning about the configured anchor peers of Org1MSP for channel my-channel1" &&
     waitForContainer "peer0.org1.com" "Anchor peer with same endpoint, skipping connecting to myself" &&
-    waitForContainer "peer0.org1.com" "Membership view has changed. peers went online:" &&
+    waitForContainer "peer0.org1.com" "Membership view has changed. peers went online:.* peer1.org1.com" &&
   waitForContainer "peer1.org1.com" "Learning about the configured anchor peers of Org1MSP for channel my-channel1 :" &&
-  waitForContainer "peer1.org1.com" "Membership view has changed. peers went online:" &&
+  waitForContainer "peer1.org1.com" "Membership view has changed. peers went online:.* peer0.org1.com" &&
   expectInvoke "cli.org1.com" "peer0.org1.com" "my-channel1" "chaincode1" \
     '{"Args":["KVContract:put", "name", "Willy Wonka"]}' \
     '{\"success\":\"OK\"}' &&
