2d566c2a19ca8a6e848328d1a9f4cef6d26a8962
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-11-04 11:59:47 +0100

    Channel management scripts - updated help
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/setup-docker/templates/fabric-docker/scripts/base-help.sh
+++ generators/setup-docker/templates/fabric-docker/scripts/base-help.sh
@@ -23,4 +23,7 @@ function printHelp() {
   echo "./fabric-docker.sh recreate"
   echo -e "\t Fresh start - it destorys whole network, certs, configs and then reruns everything."
   echo ""
+  echo "./fabric-docker.sh channel --help"
+  echo -e "\t Detailed help for channel management scripts."
+  echo ""
 }
