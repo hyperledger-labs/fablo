5a9d8c87c066759bc8b7695e07cb3b821e60d2c3
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-11-12 10:32:23 +0100

    Test java chaincode - review changes, snapshot fixes 1
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- e2e/network-07-2orgs-raft.test.js
+++ e2e/network-07-2orgs-raft.test.js
@@ -1,6 +1,6 @@
 const { performTests } = require('./performTests');
 
-const label = 'network-06-2orgs-raft';
+const label = 'network-07-2orgs-raft';
 
 describe(label, () => {
   performTests(label, 'fabrikkaConfig-2orgs-2channels-2chaincodes-tls-raft.json');
