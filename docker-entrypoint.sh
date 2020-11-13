dd0ef6956f3d7a9e5aea3d3f749670cee07ee31c
Jakub Dzikowski <jakub@bezspacji.pl>
2020-11-13 09:33:24 +0100

    preserve env in sudo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ target="/network/target"
 if [ "$(id -u)" = 0 ]; then
   echo "Root user detected, running as yeoman user"
   (sudo chown -R yeoman:yeoman "$target" &&
-    (cd "$target" && sudo -u yeoman yo --no-insight fabrikka:setup-docker "../..$config") &&
+    (cd "$target" && sudo -E -u yeoman yo --no-insight fabrikka:setup-docker "../..$config") &&
     sudo chown -R root:root "$target")
 else
   (cd "$target" && yo --no-insight fabrikka:setup-docker "../..$config")
