bce8b145d25910b4cc4f08e552d5b25379cb9477
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-11-18 17:07:46 +0100

    Create dist - fixed lint
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/splashAndVersions.js
+++ generators/splashAndVersions.js
@@ -1,21 +1,31 @@
-const fabrikkaVersion = ['alpha-0.0.1'];
+const { version } = require('../package.json');
 
-const supportedFabricVersions = ['1.3.0', '1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.4.4', '1.4.5', '1.4.6', '1.4.7', '1.4.8', `2.0`, `2.1`, `2.2.0`, `2.2.1`];
-const supportedFabrikkaVersions = ['alpha-0.0.1'];
+const supportedFabricaVersions = ['0.0.1-alpha'];
+
+const supportedFabricVersions = [
+  '1.3.0', '1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.4.4', '1.4.5', '1.4.6', '1.4.7', '1.4.8',
+  '2.0.1', '2.1.0', '2.2.0', '2.2.1',
+];
+const versionsSupportingRaft = [
+  '1.4.1', '1.4.2', '1.4.3', '1.4.4', '1.4.5', '1.4.6', '1.4.7', '1.4.8',
+  '2.0.1', '2.1.0', '2.2.0', '2.2.1',
+];
 
 function splashScreen() {
-  return `${'Fabrikka is powered by :\n'
+  return `${'Fabrica is powered by :\n'
         + ' _____        __ _                         ___  ____ _ _ \n'
         + '/  ___|      / _| |                        |  \\/  (_) | |\n'
         + '\\ `--.  ___ | |_| |___      ____ _ _ __ ___| .  . |_| | |\n'
         + ' `--. \\/ _ \\|  _| __\\ \\ /\\ / / _` | \'__/ _ \\ |\\/| | | | |\n'
         + '/\\__/ / (_) | | | |_ \\ V  V / (_| | | |  __/ |  | | | | |\n'
         + '\\____/ \\___/|_|  \\__| \\_/\\_/ \\__,_|_|  \\___\\_|  |_/_|_|_|\n'
-        + '=========================================================== v: '}${fabrikkaVersion}`;
+        + '=========================================================== v: '}${version}`;
 }
 
 module.exports = {
   splashScreen,
+  version,
   supportedFabricVersions,
-  supportedFabrikkaVersions,
+  versionsSupportingRaft,
+  supportedFabricaVersions,
 };
