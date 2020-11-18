4d67ebf0307184eef09af12632d2bb4dbaed00d0
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-11-18 17:23:33 +0100

    Create dist - improved build script (version tagging)

--- fabrica-build.sh
+++ fabrica-build.sh
@@ -3,6 +3,7 @@
 set -eu
 
 FABRICA_HOME="$(cd "$(dirname "$0")" && pwd)"
+FABRICA_VERSION=$(cat package.json  | jq -r '.version')
 
 NODE_IMAGE_TAG="12.18.0-alpine3.12"
 
@@ -10,7 +11,17 @@ COMMIT_HASH=$(git rev-parse --short HEAD)
 BUILD_DATE=$(date +'%Y-%m-%d-%H:%M:%S')
 VERSION_DETAILS="$BUILD_DATE-$COMMIT_HASH"
 
+echo "Building new image..."
+echo "   NODE_IMAGE_TAG: $NODE_IMAGE_TAG"
+echo "   FABRICA_HOME: $FABRICA_HOME"
+echo "   FABRICA_VERSION: $FABRICA_VERSION"
+echo "   VERSION_DETAILS: $VERSION_DETAILS"
+
 docker build \
   --build-arg NODE_IMAGE_TAG="$NODE_IMAGE_TAG" \
   --build-arg VERSION_DETAILS="$VERSION_DETAILS" \
   --tag fabrica "$FABRICA_HOME"
+
+docker tag fabrica $FABRICA_VERSION
+docker tag fabrica latest
+
