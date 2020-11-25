750a390537acb77f7637d454bf43c4df4ec3c4a8
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-11-25 16:28:39 +0100

    Create dist 2 - dist script renamed & improved
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- fabrica-build.sh
+++ fabrica-build.sh
@@ -3,7 +3,8 @@
 set -euo
 
 FABRICA_HOME="$(cd "$(dirname "$0")" && pwd)"
-FABRICA_VERSION=$(cat package.json  | jq -r '.version')
+# shellcheck disable=2002
+FABRICA_VERSION=$(cat "$FABRICA_HOME"/package.json | jq -r '.version')
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
 BUILD_DATE=$(date +'%Y-%m-%d-%H:%M:%S')
@@ -14,10 +15,12 @@ echo "   FABRICA_HOME: $FABRICA_HOME"
 echo "   FABRICA_VERSION: $FABRICA_VERSION"
 echo "   VERSION_DETAILS: $VERSION_DETAILS"
 
+IMAGE_BASE_NAME="softwaremill/fabrica"
+
 docker build \
   --build-arg VERSION_DETAILS="$VERSION_DETAILS" \
-  --tag fabrica "$FABRICA_HOME"
+  --tag "$IMAGE_BASE_NAME" "$FABRICA_HOME"
 
-docker tag fabrica $FABRICA_VERSION
-docker tag fabrica latest
+docker tag "$IMAGE_BASE_NAME" latest
+docker tag "$IMAGE_BASE_NAME" "softwaremill/fabrica:$FABRICA_VERSION"
 
---
+++ fabrica-release.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -euo
+
+login=$1
+pass=$2
+
+FABRICA_HOME="$(cd "$(dirname "$0")" && pwd)"
+# shellcheck disable=2002
+FABRICA_VERSION=$(cat "$FABRICA_HOME"/package.json | jq -r '.version')
+
+./fabrica-build.sh
+
+echo "Pushing to docker hub.."
+echo "   FABRICA_HOME: $FABRICA_HOME"
+echo "   FABRICA_VERSION: $FABRICA_VERSION"
+
+docker login -u "$login" -p "$pass"
+
+docker push softwaremill/fabrica:latest
+docker push softwaremill/fabrica:"$FABRICA_VERSION"
+
+docker logout
