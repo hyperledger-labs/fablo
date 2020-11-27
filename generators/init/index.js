bcc72d4d57767d76052f454f11df0fc4539a39f7
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-11-27 11:44:55 +0100

    Fabrica init - added yeoman generator
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

---
+++ generators/init/index.js
@@ -0,0 +1,18 @@
+/* eslint no-underscore-dangle: 0 */
+
+/*
+* License-Identifier: Apache-2.0
+*/
+const Generator = require('yeoman-generator');
+const simpleFabricaConfigJson = require('../../samples/fabricaConfig-1org-1channel-1chaincode');
+
+module.exports = class extends Generator {
+
+  async copySimpleFile() {
+    this.fs.writeJSON(
+        "fabrica-network-config.json",
+        simpleFabricaConfigJson,
+    );
+  }
+
+};
