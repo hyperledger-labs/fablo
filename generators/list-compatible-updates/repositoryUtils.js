e42200e80da40bc23dd04cab761564f7f44f2333
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-12-02 10:32:44 +0100

    Dist script - code review 3, lint fix + minors

--- generators/list-compatible-updates/repositoryUtils.js
+++ generators/list-compatible-updates/repositoryUtils.js
@@ -1,23 +1,23 @@
-const { repositoryTagsListUrl } = require('../config');
 const got = require('got');
+const { repositoryTagsListUrl } = require('../config');
 
 async function getAvailableTags() {
-    const params = {
-        searchParams: {
-            tag_status: 'active',
-            page_size: 1024,
-        },
-    };
-    try {
-        const response = await got(repositoryTagsListUrl, params).json();
-        return response.results.filter(version => version.name !== "latest")
-    } catch(err) {
-        console.log(`Could not check for updates. Url: '${repositoryTagsListUrl}' not available`)
-        return [];
-    }
-
+  const params = {
+    searchParams: {
+      tag_status: 'active',
+      page_size: 1024,
+    },
+  };
+  try {
+    const response = await got(repositoryTagsListUrl, params).json();
+    return response.results.filter((version) => version.name !== 'latest');
+  } catch (err) {
+    /* eslint no-console: 0 */
+    console.log(`Could not check for updates. Url: '${repositoryTagsListUrl}' not available`);
+    return [];
+  }
 }
 
 module.exports = {
-    getAvailableTags,
+  getAvailableTags,
 };
