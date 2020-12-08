2ba56ce44c5a22773771c9a28ce88be27c835b44
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-12-08 09:20:39 +0100

    Fabrica init - Code review 2
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/init/index.js
+++ generators/init/index.js
@@ -4,13 +4,23 @@
 * License-Identifier: Apache-2.0
 */
 const Generator = require('yeoman-generator');
+const chalk = require('chalk');
 const simpleFabricaConfigJson = require('../../samples/fabricaConfig-1org-1channel-1chaincode');
 
 module.exports = class extends Generator {
-  async copySimpleFile() {
+  async copySampleConfig() {
     this.fs.writeJSON(
       'fabrica-config.json',
       simpleFabricaConfigJson,
     );
+
+    this.on('end', () => {
+      this.log('===========================================================');
+      this.log(chalk.bold('Sample config file created! :)'));
+      this.log('');
+      this.log(`Please note that chaincode directory is '${simpleFabricaConfigJson.chaincodes[0].directory}'.`);
+      this.log('If it\'s empty your network won\'t run entirely.');
+      this.log('===========================================================');
+    });
   }
 };
