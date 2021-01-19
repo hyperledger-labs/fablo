d81526e1304dde78db5876e439aa1a395e33ce3e
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2021-01-19 17:43:43 +0100

    Service Discovery adjustments - fixed js lints
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/extendConfig/index.js
+++ generators/extendConfig/index.js
@@ -4,56 +4,51 @@
 * License-Identifier: Apache-2.0
 */
 const Generator = require('yeoman-generator');
-const config = require('../config');
 const utils = require('../utils/utils');
-// const buildUtil = require('../version/buildUtil');
 
 const configTransformers = require('../setup-docker/configTransformers');
 
 const ValidateGeneratorType = require.resolve('../validate');
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument('fabricaConfig', {
+      type: String,
+      required: true,
+      description: 'fabrica config file path',
+    });
 
-    constructor(args, opts) {
-        super(args, opts);
-        this.argument('fabricaConfig', {
-            type: String,
-            required: true,
-            description: 'fabrica config file path',
-        });
+    this.composeWith(ValidateGeneratorType, { arguments: [this.options.fabricaConfig] });
+  }
 
-        this.composeWith(ValidateGeneratorType, { arguments: [this.options.fabricaConfig] });
-    }
+  async writing() {
+    this.options.fabricaConfigPath = utils.getFullPathOf(
+      this.options.fabricaConfig, this.env.cwd,
+    );
 
-    async writing() {
-        this.options.fabricaConfigPath = utils.getFullPathOf(
-            this.options.fabricaConfig, this.env.cwd,
-        );
+    const {
+      networkSettings,
+      rootOrg: rootOrgJson,
+      orgs: orgsJson,
+      channels: channelsJson,
+      chaincodes: chaincodesJson,
+    } = this.fs.readJSON(this.options.fabricaConfigPath);
 
-        const {
-            networkSettings,
-            rootOrg: rootOrgJson,
-            orgs: orgsJson,
-            channels: channelsJson,
-            chaincodes: chaincodesJson,
-        } = this.fs.readJSON(this.options.fabricaConfigPath);
+    const capabilities = configTransformers.getNetworkCapabilities(networkSettings.fabricVersion);
+    const rootOrg = configTransformers.transformRootOrgConfig(rootOrgJson);
+    const orgs = configTransformers.transformOrgConfigs(orgsJson);
+    const channels = configTransformers.transformChannelConfigs(channelsJson, orgs);
+    const chaincodes = configTransformers.transformChaincodesConfig(chaincodesJson, channels);
 
-        const capabilities = configTransformers.getNetworkCapabilities(networkSettings.fabricVersion);
-        const rootOrg = configTransformers.transformRootOrgConfig(rootOrgJson);
-        const orgs = configTransformers.transformOrgConfigs(orgsJson);
-        const channels = configTransformers.transformChannelConfigs(channelsJson, orgs);
-        const chaincodes = configTransformers.transformChaincodesConfig(chaincodesJson, channels);
+    const transformedConfig = {
+      capabilities,
+      rootOrg,
+      orgs,
+      channels,
+      chaincodes,
+    };
 
-        const transformedConfig = {
-            capabilities,
-            rootOrg,
-            orgs,
-            channels,
-            chaincodes
-        }
-
-        this.log(JSON.stringify(transformedConfig, null, 4));
-    }
-
-
-}
+    this.log(JSON.stringify(transformedConfig, null, 4));
+  }
+};
