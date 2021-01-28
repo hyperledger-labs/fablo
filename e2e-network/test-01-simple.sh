398476712cacea4e98db1f0da427f866c5aad779
Jakub Dzikowski <jakub@bezspacji.pl>
2021-01-28 14:22:46 +0100

    move anchor peer tests before chaincode tests
    Signed-off-by: Jakub Dzikowski <jakub.dzikowski@softwaremill.com>

--- e2e-network/test-01-simple.sh
+++ e2e-network/test-01-simple.sh
@@ -51,14 +51,14 @@ waitForContainer "ca.root.com" "Listening on http://0.0.0.0:7054" &&
   waitForContainer "peer0.org1.com" "Joining gossip network of channel my-channel1 with 1 organizations" &&
   waitForContainer "peer1.org1.com" "Joining gossip network of channel my-channel1 with 1 organizations" &&
 
-  waitForChaincode "cli.org1.com" "peer0.org1.com:7060" "my-channel1" "chaincode1" "0.0.1" &&
-  waitForChaincode "cli.org1.com" "peer1.org1.com:7061" "my-channel1" "chaincode1" "0.0.1" &&
-
   waitForContainer "peer0.org1.com" "Learning about the configured anchor peers of Org1MSP for channel my-channel1" &&
   waitForContainer "peer0.org1.com" "Anchor peer with same endpoint, skipping connecting to myself" &&
   waitForContainer "peer0.org1.com" "Membership view has changed. peers went online:.*peer1.org1.com:7061" &&
   waitForContainer "peer1.org1.com" "Learning about the configured anchor peers of Org1MSP for channel my-channel1" &&
   waitForContainer "peer1.org1.com" "Membership view has changed. peers went online:.*peer0.org1.com:7060" &&
+
+  waitForChaincode "cli.org1.com" "peer0.org1.com:7060" "my-channel1" "chaincode1" "0.0.1" &&
+  waitForChaincode "cli.org1.com" "peer1.org1.com:7061" "my-channel1" "chaincode1" "0.0.1" &&
 
   expectInvoke "cli.org1.com" "peer0.org1.com:7060" "my-channel1" "chaincode1" \
     '{"Args":["KVContract:put", "name", "Willy Wonka"]}' \
