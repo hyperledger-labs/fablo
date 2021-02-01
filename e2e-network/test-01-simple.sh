ed90d605a7b53b3c8dbd972ad07c8acaaccfddc0
Jakub Dzikowski <jakub@bezspacji.pl>
2021-02-01 15:34:28 +0100

    typo fix
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- e2e-network/test-01-simple.sh
+++ e2e-network/test-01-simple.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 TEST_TMP="$(rm -rf "$0.tmpdir" && mkdir -p "$0.tmpdir" && (cd "$0.tmpdir" && pwd))"
 TEST_LOGS="$(mkdir -p "$0.logs" && (cd "$0.logs" && pwd))"
-FABRIKKA_HOME="$TEST_TMP/../.."
+FABRICA_HOME="$TEST_TMP/../.."
 
 # testing relative path
-CONFIG="../../samples/fabrikkaConfig-1org-1channel-2chaincodes.json"
+CONFIG="../../samples/fabricaConfig-1org-1channel-1chaincode.json"
 
 networkUpAsync() {
-  "$FABRIKKA_HOME/fabrikka-build.sh" &&
-    (cd "$TEST_TMP" && "$FABRIKKA_HOME/fabrikka.sh" generate "$CONFIG") &&
-    (cd "$TEST_TMP" && "$FABRIKKA_HOME/fabrikka.sh" up &)
+  "$FABRICA_HOME/fabrica-build.sh" &&
+    (cd "$TEST_TMP" && "$FABRICA_HOME/fabrica.sh" generate "$CONFIG") &&
+    (cd "$TEST_TMP" && "$FABRICA_HOME/fabrica.sh" up &)
 }
 
 dumpLogs() {
@@ -27,7 +27,7 @@ networkDown() {
     dumpLogs "peer0.org1.com" &&
     dumpLogs "peer1.org1.com" &&
     dumpLogs "cli.org1.com" &&
-    (cd "$TEST_TMP" && "$FABRIKKA_HOME/fabrikka.sh" down)
+    (cd "$TEST_TMP" && "$FABRICA_HOME/fabrica.sh" down)
 }
 
 waitForContainer() {
@@ -44,33 +44,46 @@ expectInvoke() {
 
 networkUpAsync
 
+# shellcheck disable=2015
 waitForContainer "ca.root.com" "Listening on http://0.0.0.0:7054" &&
   waitForContainer "orderer0.root.com" "Created and starting new chain my-channel1" &&
   waitForContainer "ca.org1.com" "Listening on http://0.0.0.0:7054" &&
   waitForContainer "peer0.org1.com" "Joining gossip network of channel my-channel1 with 1 organizations" &&
   waitForContainer "peer1.org1.com" "Joining gossip network of channel my-channel1 with 1 organizations" &&
 
-  waitForChaincode "cli.org1.com" "peer0.org1.com" "my-channel1" "chaincode1" "0.0.1" &&
-  waitForChaincode "cli.org1.com" "peer1.org1.com" "my-channel1" "chaincode1" "0.0.1" &&
-  waitForChaincode "cli.org1.com" "peer0.org1.com" "my-channel1" "chaincode2" "0.0.1" &&
-  waitForChaincode "cli.org1.com" "peer1.org1.com" "my-channel1" "chaincode2" "0.0.1" &&
+  waitForChaincode "cli.org1.com" "peer0.org1.com:7060" "my-channel1" "chaincode1" "0.0.1" &&
+  waitForChaincode "cli.org1.com" "peer1.org1.com:7061" "my-channel1" "chaincode1" "0.0.1" &&
 
   waitForContainer "peer0.org1.com" "Learning about the configured anchor peers of Org1MSP for channel my-channel1" &&
   waitForContainer "peer0.org1.com" "Anchor peer with same endpoint, skipping connecting to myself" &&
-  waitForContainer "peer0.org1.com" "Membership view has changed. peers went online:.* peer1.org1.com" &&
-  waitForContainer "peer1.org1.com" "Learning about the configured anchor peers of Org1MSP for channel my-channel1 :" &&
-  waitForContainer "peer1.org1.com" "Membership view has changed. peers went online:.* peer0.org1.com" &&
+  waitForContainer "peer0.org1.com" "Membership view has changed. peers went online:.*peer1.org1.com:7061" &&
+  waitForContainer "peer1.org1.com" "Learning about the configured anchor peers of Org1MSP for channel my-channel1" &&
+  waitForContainer "peer1.org1.com" "Membership view has changed. peers went online:.*peer0.org1.com:7060" &&
 
-  expectInvoke "cli.org1.com" "peer0.org1.com" "my-channel1" "chaincode1" \
+  expectInvoke "cli.org1.com" "peer0.org1.com:7060" "my-channel1" "chaincode1" \
     '{"Args":["KVContract:put", "name", "Willy Wonka"]}' \
     '{\"success\":\"OK\"}' &&
-  expectInvoke "cli.org1.com" "peer1.org1.com" "my-channel1" "chaincode1" \
+  expectInvoke "cli.org1.com" "peer1.org1.com:7061" "my-channel1" "chaincode1" \
     '{"Args":["KVContract:get", "name"]}' \
     '{\"success\":\"Willy Wonka\"}' &&
-  expectInvoke "cli.org1.com" "peer0.org1.com" "my-channel1" "chaincode2" \
-    '{"Args":["PokeballContract:createPokeball", "id1", "Pokeball 1"]}' \
-    'status:200' &&
-  expectInvoke "cli.org1.com" "peer1.org1.com" "my-channel1" "chaincode2" \
-    '{"Args":["PokeballContract:readPokeball", "id1"]}' \
-    '{\"value\":\"Pokeball 1\"}' &&
+
+  (cd "$TEST_TMP" && "$FABRICA_HOME/fabrica.sh" reboot) &&
+  waitForChaincode "cli.org1.com" "peer0.org1.com:7060" "my-channel1" "chaincode1" "0.0.1" &&
+  waitForChaincode "cli.org1.com" "peer1.org1.com:7061" "my-channel1" "chaincode1" "0.0.1" &&
+
+  expectInvoke "cli.org1.com" "peer0.org1.com:7060" "my-channel1" "chaincode1" \
+    '{"Args":["KVContract:get", "name"]}' \
+    '{\"error\":\"NOT_FOUND\"}' &&
+  expectInvoke "cli.org1.com" "peer0.org1.com:7060" "my-channel1" "chaincode1" \
+    '{"Args":["KVContract:put", "name", "James Bond"]}' \
+    '{\"success\":\"OK\"}' &&
+
+  (cd "$TEST_TMP" && "$FABRICA_HOME/fabrica.sh" chaincode upgrade "chaincode1" "0.0.2") &&
+  waitForChaincode "cli.org1.com" "peer0.org1.com:7060" "my-channel1" "chaincode1" "0.0.2" &&
+  waitForChaincode "cli.org1.com" "peer1.org1.com:7061" "my-channel1" "chaincode1" "0.0.2" &&
+
+  expectInvoke "cli.org1.com" "peer1.org1.com:7061" "my-channel1" "chaincode1" \
+    '{"Args":["KVContract:get", "name"]}' \
+    '{\"success\":\"James Bond\"}' &&
+
   networkDown || (networkDown && exit 1)
