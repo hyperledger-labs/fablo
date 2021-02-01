67928421d16489ea18a11c8e95ed653b860184a7
Jakub Dzikowski <jakub@bezspacji.pl>
2021-02-01 08:09:46 +0100

    TestCommands in separate file

--- e2e/fabricaCommands.test.js
+++ e2e/fabricaCommands.test.js
@@ -1,128 +1,88 @@
-const { execSync } = require('child_process');
+const TestCommands = require('./TestCommands').default;
 const currentFabricaVersion = require('../package.json').version;
 
-const commandOutput = (status, output) => ({
-  status,
-  output,
-  outputJson: () => JSON.parse(output),
-});
-
-const executeCommand = (c, noConsole = false) => {
-  // eslint-disable-next-line no-console
-  const log = !noConsole ? (out) => console.log(out) : () => {};
-  try {
-    log(c);
-    const output = execSync(c, { encoding: 'utf-8' });
-    log(output);
-    return commandOutput(0, output);
-  } catch (e) {
-    const output = (e.output || []).join('');
-    // eslint-disable-next-line no-console
-    console.error(output);
-    return commandOutput(e.status, output);
-  }
-};
-
-const success = () => expect.objectContaining({ status: 0 });
-const failure = () => expect.objectContaining({ status: 1 });
-
-const workdir = './e2e/__tmp__/commands-tests';
-const relativeRoot = '../../..';
-
-const fabricaExec = (command) => executeCommand(`cd ${workdir} && ${relativeRoot}/fabrica.sh ${command}`);
-
-const getFiles = () => executeCommand(`find '${workdir}' -type f`)
-  .output
-  .split('\n')
-  .filter((s) => !!s.length)
-  .sort();
-
-const getFileContent = (file) => executeCommand(`cat ${file}`).output;
-
-const cleanupWorkdir = () => {
-  execSync(`rm -rf ${workdir} ; mkdir -p ${workdir}`);
-};
+const commands = new TestCommands('./e2e/__tmp__/commands-tests', '../../..');
 
 describe('init', () => {
-  beforeEach(cleanupWorkdir);
+  beforeEach(() => commands.cleanupWorkdir());
 
   it('should init simple fabrica config', () => {
     // When
-    const commandResult = fabricaExec('init');
+    const commandResult = commands.fabricaExec('init');
 
     // Then
-    expect(commandResult).toEqual(success());
+    expect(commandResult).toEqual(TestCommands.success());
     expect(commandResult.output).toContain('Sample config file created! :)');
     expect(commandResult.output).toContain('Chaincode directory is \'./chaincodes/chaincode-kv-node\'.');
     expect(commandResult.output).toContain('If it\'s empty your network won\'t run entirely.');
-    expect(getFiles()).toEqual(['./e2e/__tmp__/commands-tests/fabrica-config.json']);
-    expect(getFileContent(`${workdir}/fabrica-config.json`)).toMatchSnapshot();
+    expect(commands.getFiles()).toEqual(['./e2e/__tmp__/commands-tests/fabrica-config.json']);
+    expect(commands.getFileContent('fabrica-config.json')).toMatchSnapshot();
   });
 });
 
 describe('use', () => {
-  beforeEach(cleanupWorkdir);
+  beforeEach(() => commands.cleanupWorkdir());
 
   it('should display versions', () => {
     // When
-    const commandResult = fabricaExec('use');
+    const commandResult = commands.fabricaExec('use');
 
     // Then
-    expect(commandResult).toEqual(success());
+    expect(commandResult).toEqual(TestCommands.success());
     expect(commandResult.output).toContain('0.0.1');
     expect(commandResult.output).toContain(`${currentFabricaVersion} <== current`);
-    expect(getFiles()).toEqual([]);
+    expect(commands.getFiles()).toEqual([]);
   });
 });
 
 describe('validate', () => {
-  beforeEach(cleanupWorkdir);
+  beforeEach(() => commands.cleanupWorkdir());
 
   it('should validate default config', () => {
     // Given
-    fabricaExec('init');
+    commands.fabricaExec('init');
 
     // When
-    const commandResult = fabricaExec('validate');
+    const commandResult = commands.fabricaExec('validate');
 
     // Then
-    expect(commandResult).toEqual(success());
+    expect(commandResult).toEqual(TestCommands.success());
     expect(commandResult.output).toContain('Validation errors count: 0');
     expect(commandResult.output).toContain('Validation warnings count: 0');
-    expect(getFiles()).toEqual(['./e2e/__tmp__/commands-tests/fabrica-config.json']);
+    expect(commands.getFiles()).toEqual(['./e2e/__tmp__/commands-tests/fabrica-config.json']);
   });
 
   it('should validate custom config', () => {
     // Given
-    const fabricaConfig = `${relativeRoot}/samples/fabricaConfig-2orgs-2channels-2chaincodes-tls-raft.json`;
+    const fabricaConfig = `${commands.relativeRoot}/samples/fabricaConfig-2orgs-2channels-2chaincodes-tls-raft.json`;
 
     // When
-    const commandResult = fabricaExec(`validate ${fabricaConfig}`);
+    const commandResult = commands.fabricaExec(`validate ${fabricaConfig}`);
 
     // Then
-    expect(commandResult).toEqual(success());
+    expect(commandResult).toEqual(TestCommands.success());
     expect(commandResult.output).toContain('Validation errors count: 0');
     expect(commandResult.output).toContain('Validation warnings count: 0');
-    expect(getFiles()).toEqual([]);
+    expect(commands.getFiles()).toEqual([]);
   });
 
   it('should fail to validate if config file is missing', () => {
-    const commandResult = fabricaExec('validate');
+    const commandResult = commands.fabricaExec('validate');
 
     // Then
-    expect(commandResult).toEqual(failure());
+    expect(commandResult).toEqual(TestCommands.failure());
     expect(commandResult.output).toContain('commands-tests/fabrica-config.json does not exist\n');
-    expect(getFiles()).toEqual([]);
+    expect(commands.getFiles()).toEqual([]);
   });
 });
 
 describe('version', () => {
   it('should print version information', () => {
     // When
-    const commandResult = fabricaExec('version');
+    const commandResult = commands.fabricaExec('version');
 
     // Then
-    expect(commandResult).toEqual(success());
+    expect(commandResult).toEqual(TestCommands.success());
     expect(commandResult.outputJson()).toEqual(expect.objectContaining({
       version: currentFabricaVersion,
       build: expect.stringMatching(/.*/),
@@ -131,11 +91,11 @@ describe('version', () => {
 
   it('should print verbose version information', () => {
     // When
-    const commandResult1 = fabricaExec('version -v');
-    const commandResult2 = fabricaExec('version --verbose');
+    const commandResult1 = commands.fabricaExec('version -v');
+    const commandResult2 = commands.fabricaExec('version --verbose');
 
     // Then
-    expect(commandResult1).toEqual(success());
+    expect(commandResult1).toEqual(TestCommands.success());
     expect(commandResult1.outputJson()).toEqual(expect.objectContaining({
       version: currentFabricaVersion,
       build: expect.stringMatching(/.*/),
@@ -149,5 +109,3 @@ describe('version', () => {
     expect(commandResult1.output).toEqual(commandResult2.output);
   });
 });
-
-// TODO describe(printConfig)
