5530f2df8cc88d98830bbe16b3eaf56062d54c0a
Jakub Dzikowski <jakub@bezspacji.pl>
2021-02-01 06:49:31 +0100

    $FABRICA_NETWORK_ROOT variable is back in some cases
    Signed-off-by: Jakub Dzikowski <jakub.dzikowski@softwaremill.com>

--- generators/setup-docker/templates/fabric-docker.sh
+++ generators/setup-docker/templates/fabric-docker.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-FABRICA_WORKSPACE="$(cd "$(dirname "$0")" && pwd)"
+FABRICA_NETWORK_ROOT="$(cd "$(dirname "$0")" && pwd)"
 
-source "$FABRICA_WORKSPACE/fabric-docker/scripts/base-help.sh"
-source "$FABRICA_WORKSPACE/fabric-docker/scripts/base-functions.sh"
-source "$FABRICA_WORKSPACE/fabric-docker/scripts/chaincode-functions.sh"
-source "$FABRICA_WORKSPACE/fabric-docker/commands-generated.sh"
-source "$FABRICA_WORKSPACE/fabric-docker/.env"
+source "$FABRICA_NETWORK_ROOT/fabric-docker/scripts/base-help.sh"
+source "$FABRICA_NETWORK_ROOT/fabric-docker/scripts/base-functions.sh"
+source "$FABRICA_NETWORK_ROOT/fabric-docker/scripts/chaincode-functions.sh"
+source "$FABRICA_NETWORK_ROOT/fabric-docker/commands-generated.sh"
+source "$FABRICA_NETWORK_ROOT/fabric-docker/.env"
 
 function networkUp() {
   generateArtifacts
