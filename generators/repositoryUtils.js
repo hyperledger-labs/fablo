e6bacc93986acddc6511ad3f3315ca3bb7289e63
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2021-02-12 09:58:26 +0100

    fix: Named versions as last - after review
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/repositoryUtils.js
+++ generators/repositoryUtils.js
@@ -1,6 +1,37 @@
 const got = require('got');
 const { repositoryTagsListUrl } = require('./config');
 
+const incrementVersionFragments = (versionFragment) => {
+  if (versionFragment.includes('-')) {
+    const splitted = versionFragment.split('-');
+    return `${+splitted[0] + 100000}-${splitted[1]}`;
+  }
+  return +versionFragment + 100000;
+};
+
+const decrementVersionFragments = (incrementedVersionFragment) => {
+  if (incrementedVersionFragment.includes('-')) {
+    const splitted = incrementedVersionFragment.split('-');
+    return `${+splitted[0] - 100000}-${splitted[1]}`;
+  }
+  return +incrementedVersionFragment - 100000;
+};
+
+const namedVersionsAsLast = (a, b) => {
+  if (/[a-z]/i.test(a) && !/[a-z]/i.test(b)) return -1;
+  if (/[a-z]/i.test(b) && !/[a-z]/i.test(a)) return 1;
+  if (a.toString() < b.toString()) return -1;
+  if (a.toString() > b.toString()) return 1;
+  return 0;
+};
+
+function sortVersions(versions) {
+  return versions
+    .map((a) => a.split('.').map(incrementVersionFragments).join('.')).sort(namedVersionsAsLast)
+    .map((a) => a.split('.').map(decrementVersionFragments).join('.'))
+    .reverse();
+}
+
 async function getAvailableTags() {
   const params = {
     searchParams: {
@@ -9,22 +40,15 @@ async function getAvailableTags() {
     },
   };
   try {
-    const response = await got(repositoryTagsListUrl, params).json();
-    return response.results
-        .filter((version) => version.name !== 'latest')
-        .map(tag => tag.name);
+    const versionNames = (await got(repositoryTagsListUrl, params).json()).results
+      .filter((version) => version.name !== 'latest')
+      .map((tag) => tag.name);
+    return sortVersions(versionNames);
   } catch (err) {
     /* eslint no-console: 0 */
     console.log(`Could not check for updates. Url: '${repositoryTagsListUrl}' not available`);
     return [];
   }
-}
-
-function sortVersions(versionsList) {
-  return versionsList
-    .map((a) => a.split('.').map((n) => +n + 100000).join('.')).sort()
-    .map((a) => a.split('.').map((n) => +n - 100000).join('.'))
-    .reverse();
 }
 
 module.exports = {
