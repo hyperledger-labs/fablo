4ea4355cfdbf16349995ceb62d5400ea6e0c8ac2
Jakub Dzikowski <jakub@bezspacji.pl>
2021-02-19 14:15:22 +0100

    add getHistory smart contract for node chaincode
    Signed-off-by: Jakub Dzikowski <jakub.dzikowski@softwaremill.com>

--- samples/chaincodes/chaincode-kv-node/index.js
+++ samples/chaincodes/chaincode-kv-node/index.js
@@ -19,6 +19,31 @@ class KVContract extends Contract {
     if (!buffer || !buffer.length) return { error: 'NOT_FOUND' };
     return { success: buffer.toString() };
   }
+
+  async getHistory(ctx, key) {
+    const iterator = await ctx.stub.getHistoryForKey(key);
+    const results = [];
+
+    for (let i = 0; i < 100; i += 1) { // limit to 100 results
+      // eslint-disable-next-line no-await-in-loop
+      const next = await iterator.next();
+      if (!next || !next.value) break;
+
+      const {
+        is_delete: isDelete, value, timestamp, tx_id: txId,
+      } = next.value;
+
+      const date = new Date(timestamp.seconds.low * 1000 + timestamp.nanos / 1000000);
+      results.push({
+        isDelete,
+        value: value.toBuffer().toString(),
+        timestamp: date.toISOString(),
+        txId,
+      });
+    }
+
+    return { success: results };
+  }
 }
 
 exports.contracts = [KVContract];
