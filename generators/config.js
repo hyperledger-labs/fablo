fcc75c67521d810d6bb8d135d10ab2a914a20994
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2021-03-24 11:47:43 +0100

    hlf 2x basic support - working test 01
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/config.js
+++ generators/config.js
@@ -13,11 +13,14 @@ const getVersionFromSchemaUrl = (url) => {
 const isFabricaVersionSupported = (versionName) => versionName.startsWith(supportedVersionPrefix);
 
 const supportedFabricVersions = [
-  '1.3.0', '1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.4.4', '1.4.5', '1.4.6', '1.4.7', '1.4.8', '1.4.9', '1.4.10', '1.4.11',
+  '1.3.0',
+  '1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.4.4', '1.4.5', '1.4.6', '1.4.7', '1.4.8', '1.4.9', '1.4.10', '1.4.11',
+  '2.0.1', '2.1.0', '2.1.1', '2.2.0', '2.2.1',
 ];
 
 const versionsSupportingRaft = [
   '1.4.1', '1.4.2', '1.4.3', '1.4.4', '1.4.5', '1.4.6', '1.4.7', '1.4.8', '1.4.9', '1.4.10', '1.4.11',
+  '2.0.1', '2.1.0', '2.1.1', '2.2.0', '2.2.1',
 ];
 
 const splashScreen = () => `${'Fabrica is powered by :\n'
