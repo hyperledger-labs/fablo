05d1ae9d43fd1d24367df1d1a3aed3f4687ada56
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2021-03-25 10:52:23 +0100

    hlf 2x basic support - added sleep in test-02

--- e2e-network/test-02-raft-2orgs.sh
+++ e2e-network/test-02-raft-2orgs.sh
@@ -88,6 +88,7 @@ waitForContainer "ca.root.com" "Listening on http://0.0.0.0:7054" &&
   (cd "$TEST_TMP" && "$FABRICA_HOME/fabrica.sh" stop && "$FABRICA_HOME/fabrica.sh" start) &&
   waitForChaincode "cli.org1.com" "peer1.org1.com:7061" "my-channel2" "chaincode1" "0.0.1" &&
   waitForChaincode "cli.org2.com" "peer1.org2.com:7071" "my-channel2" "chaincode1" "0.0.1" &&
+  sleep 10 &&
 
   (cd "$TEST_TMP" && "$FABRICA_HOME/fabrica.sh" chaincode upgrade "chaincode1" "0.0.2") &&
   waitForChaincode "cli.org1.com" "peer1.org1.com:7061" "my-channel2" "chaincode1" "0.0.2" &&
