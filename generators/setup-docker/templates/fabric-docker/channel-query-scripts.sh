d5e7243da0add6a5b472c171f1c6aeb71a41390b
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2021-03-26 13:27:49 +0100

    channel query scripts - moved to fixed peer names

--- generators/setup-docker/templates/fabric-docker/channel-query-scripts.sh
+++ generators/setup-docker/templates/fabric-docker/channel-query-scripts.sh
@@ -17,77 +17,42 @@ elif [ "$1" = "list" ] && [ "$2" = "<%= org.name.toLowerCase(); %>" ] && [ "$3" 
   <% } %>
 <% })}) %>
 
-<% channels.forEach(function(channel) { channel.orgs.forEach(function(org) { %>
-elif [ "$1" = "getinfo" ] && [ "$2" = "<%= channel.name %>" ] && [ "$3" = "<%= org.mspName.toLowerCase(); %>" ] && [ -n "$4" ]; then
-  PEER_NAME=$4
+<% channels.forEach(function(channel) { channel.orgs.forEach(function(org) { org.peers.forEach(function(peer) { %>
+elif [ "$1" = "getinfo" ] && [ "$2" = "<%= channel.name %>" ] && [ "$3" = "<%= org.name.toLowerCase(); %>" ] && [ "$4" = "<%= peer.name %>" ]; then
   <% if(!networkSettings.tls) { -%>
-    peerChannelGetInfo "<%= channel.name %>" "<%= org.cli.address %>" "${PEER_NAME}.<%= org.domain %>:7051"
+    peerChannelGetInfo "<%= channel.name %>" "<%= org.cli.address %>" "<%= peer.fullAddress %>"
   <% } else { -%>
-    peerChannelGetInfoTls "<%= channel.name %>" "<%= org.cli.address %>" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelGetInfoTls "<%= channel.name %>" "<%= org.cli.address %>" "<%= peer.fullAddress %>" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
-elif [ "$1" = "fetch" ] && [ "$2" = "config" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 5 ]; then
-  FILE_NAME=$5
+elif [ "$1" = "fetch" ] && [ "$2" = "config" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.name.toLowerCase(); %>" ] && [ "$5" = "<%= peer.name %>" ] && [ "$#" = 6 ]; then
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchConfigDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}"
+    peerChannelFetchConfig "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "<%= peer.fullAddress %>"
   <% } else { -%>
-    peerChannelFetchConfigDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchConfigTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "<%= peer.fullAddress %>" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
-elif [ "$1" = "fetch" ] && [ "$2" = "config" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 6 ]; then
-  FILE_NAME=$6
-  PEER_NAME=$7
-  <% if(!networkSettings.tls) { -%>
-    peerChannelFetchConfig "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051"
-  <% } else { -%>
-    peerChannelFetchConfigTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
-  <% } %>
-elif [ "$1" = "fetch" ] && [ "$2" = "lastBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 5 ]; then
+elif [ "$1" = "fetch" ] && [ "$2" = "lastBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.name.toLowerCase(); %>" ] && [ "$5" = "<%= peer.name %>" ] && [ "$#" = 6 ]; then
   FILE_NAME=$6
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchLastBlockDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}"
+    peerChannelFetchLastBlock "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "<%= peer.fullAddress %>"
   <% } else { -%>
-    peerChannelFetchLastBlockDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchLastBlockTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "<%= peer.fullAddress %>" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
-elif [ "$1" = "fetch" ] && [ "$2" = "lastBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 6 ]; then
-  FILE_NAME=$6
-  PEER_NAME=$7
-  <% if(!networkSettings.tls) { -%>
-    peerChannelFetchLastBlock "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051"
-  <% } else { -%>
-    peerChannelFetchLastBlockTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
-  <% } %>
-elif [ "$1" = "fetch" ] && [ "$2" = "firstBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 5 ]; then
+elif [ "$1" = "fetch" ] && [ "$2" = "firstBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.name.toLowerCase(); %>" ] && [ "$5" = "<%= peer.name %>" ] && [ "$#" = 6 ]; then
   FILE_NAME=$6
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchFirstBlockDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}"
+    peerChannelFetchFirstBlockDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "<%= peer.fullAddress %>"
   <% } else { -%>
-    peerChannelFetchFirstBlockDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchFirstBlockDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "<%= peer.fullAddress %>" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
-elif [ "$1" = "fetch" ] && [ "$2" = "firstBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 6 ]; then
-  FILE_NAME=$6
-  PEER_NAME=$7
-  <% if(!networkSettings.tls) { -%>
-    peerChannelFetchFirstBlockDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051"
-  <% } else { -%>
-    peerChannelFetchFirstBlockDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
-  <% } %>
-elif [ "$1" = "fetch" ] && [ "$2" = "block" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 6 ]; then
+elif [ "$1" = "fetch" ] && [ "$2" = "block" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.name.toLowerCase(); %>" ] && [ "$5" = "<%= peer.name %>" ] && [ "$#" = 7 ]; then
   FILE_NAME=$6
   BLOCK_NUMBER=$7
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchBlockDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}"
+    peerChannelFetchBlock "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "<%= peer.fullAddress %>"
   <% } else { -%>
-    peerChannelFetchBlockDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchBlockTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "<%= peer.fullAddress %>" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
-elif [ "$1" = "fetch" ] && [ "$2" = "block" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 7 ]; then
-  FILE_NAME=$6
-  BLOCK_NUMBER=$7
-  PEER_NAME=$8
-  <% if(!networkSettings.tls) { -%>
-    peerChannelFetchBlock "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "${PEER_NAME}.<%= org.domain %>:7051"
-  <% } else { -%>
-    peerChannelFetchBlockTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
-  <% } %>
-<% })}) %>
+<% })})}) %>
 else
   printChannelsHelp
 fi
@@ -95,25 +60,25 @@ fi
 }
 
 function printChannelsHelp() {
-  echo "Channel managment commands:"
+  echo "Channel management commands:"
   echo ""
   <% orgs.forEach(function(org) { org.peers.forEach(function(peer) { %>
     echo "fabrica channel list <%= org.name.toLowerCase(); %> <%= peer.name %>"
     echo -e "\t List channels on '<%= peer.name %>' of '<%= org.name %>'".
     echo ""
   <% })}) %>
-  <% channels.forEach(function(channel) { channel.orgs.forEach(function(org) { %>
-    echo "fabrica channel getinfo <%= channel.name %> <%= org.name.toLowerCase(); %> <peer0>"
-    echo -e "\t Get channel info on given peer of <%= org.name %>".
+  <% channels.forEach(function(channel) { channel.orgs.forEach(function(org) { org.peers.forEach(function(peer) { %>
+    echo "fabrica channel getinfo <%= channel.name %> <%= org.name.toLowerCase(); %> <%= peer.name %>"
+    echo -e "\t Get channel info on '<%= peer.name %>' of '<%= org.name %>'".
     echo ""
-    echo "fabrica channel fetch config <%= channel.name %> <%= org.name.toLowerCase(); %> <fileName.json> [peer0]"
-    echo -e "\t Download latest config block to current dir. By default it uses first peer of <%= org.name %>".
+    echo "fabrica channel fetch config <%= channel.name %> <%= org.name.toLowerCase(); %> <%= peer.name %> <fileName.json>"
+    echo -e "\t Download latest config block to current dir. Uses first peer '<%= peer.name %>' of '<%= org.name %>'".
     echo ""
-    echo "fabrica channel fetch lastBlock <%= channel.name %> <%= org.name.toLowerCase(); %> <fileName.json> [peer0]"
-    echo -e "\t Download last, decrypted block to current dir. By default it uses first peer of <%= org.name %>".
+    echo "fabrica channel fetch lastBlock <%= channel.name %> <%= org.name.toLowerCase(); %> <%= peer.name %> <fileName.json>"
+    echo -e "\t Download last, decrypted block to current dir. Uses first peer '<%= peer.name %>' of '<%= org.name %>'".
     echo ""
-    echo "fabrica channel fetch firstBlock <%= channel.name %> <%= org.name.toLowerCase(); %> <fileName.json> [peer0]"
-    echo -e "\t Download first, decrypted block to current dir. By default it uses first peer of <%= org.name %>".
+    echo "fabrica channel fetch firstBlock <%= channel.name %> <%= org.name.toLowerCase(); %> <%= peer.name %> <fileName.json>"
+    echo -e "\t Download first, decrypted block to current dir. Uses first peer '<%= peer.name %>' of '<%= org.name %>'".
     echo ""
-  <% })}) %>
+  <% })})}) %>
 }
