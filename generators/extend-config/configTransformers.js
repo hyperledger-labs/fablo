d7f310592cc7613e2939a1d521e6167bb73b261d
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2021-03-26 12:47:04 +0100

    channel query scripts - fixed peer list
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

---
+++ generators/extend-config/configTransformers.js
@@ -0,0 +1,274 @@
+const _ = require('lodash');
+const { version } = require('../repositoryUtils');
+
+function createPrivateCollectionConfig(fabricVersion, channel, name, orgNames) {
+  // We need only orgs that can have access to private data
+  const relevantOrgs = (channel.orgs || []).filter((o) => !!orgNames.find((n) => n === o.name));
+  if (relevantOrgs.length < orgNames.length) { throw new Error(`Cannot find all orgs for names ${orgNames}`); }
+
+  const policy = `OR(${relevantOrgs.map((o) => `'${o.mspName}.member'`).join(',')})`;
+  const peerCounts = relevantOrgs.map((o) => (o.peers || []).length);
+  const totalPeers = peerCounts.reduce((a, b) => a + b, 0);
+
+  // We need enough peers to exceed one org (max in org + 1) and up to totalPeers - 1
+  const requiredPeerCount = Math.min(
+    totalPeers - 1,
+    peerCounts.reduce((a, b) => Math.max(a, b), 0) + 1,
+  );
+
+  const maxPeerCount = Math.max(
+    requiredPeerCount,
+    Math.min(requiredPeerCount * 2, totalPeers - 1),
+  );
+
+  const memberOnlyRead = version(fabricVersion).isGreaterOrEqual('1.4.0') ? { memberOnlyRead: true } : {};
+  const memberOnlyWrite = version(fabricVersion).isGreaterOrEqual('2.0.0') ? { memberOnlyWrite: true } : {};
+
+  return {
+    name,
+    policy,
+    requiredPeerCount,
+    maxPeerCount,
+    blockToLive: 0,
+    ...memberOnlyRead,
+    ...memberOnlyWrite,
+  };
+}
+
+function transformChaincodesConfig(fabricVersion, chaincodes, transformedChannels) {
+  return chaincodes.map((chaincode) => {
+    const channel = transformedChannels.find((c) => c.key === chaincode.channel);
+    if (!channel) throw new Error(`No matching channel with key '${chaincode.channel}'`);
+
+    const privateData = (chaincode.privateData || [])
+      .map((d) => createPrivateCollectionConfig(fabricVersion, channel, d.name, d.orgNames));
+
+    return {
+      directory: chaincode.directory,
+      name: chaincode.name,
+      version: chaincode.version,
+      lang: chaincode.lang,
+      channel,
+      init: chaincode.init,
+      endorsement: chaincode.endorsement,
+      instantiatingOrg: channel.instantiatingOrg,
+      privateData,
+    };
+  });
+}
+
+function transformOrderersConfig(ordererJsonConfigFormat, rootDomainJsonConfigFormat) {
+  const type = ordererJsonConfigFormat.type === 'raft' ? 'etcdraft' : ordererJsonConfigFormat.type;
+
+  return Array(ordererJsonConfigFormat.instances)
+    .fill()
+    .map((x, i) => i)
+    .map((i) => {
+      const name = `${ordererJsonConfigFormat.prefix}${i}`;
+      const address = `${name}.${rootDomainJsonConfigFormat}`;
+      const port = 7050 + i;
+      return {
+        name,
+        domain: rootDomainJsonConfigFormat,
+        address,
+        consensus: type,
+        port,
+        fullAddress: `${address}:${port}`,
+      };
+    });
+}
+
+function transformCaConfig(caJsonFormat, orgName, orgDomainJsonFormat, caExposePort) {
+  const address = `${caJsonFormat.prefix}.${orgDomainJsonFormat}`;
+  const port = 7054;
+  return {
+    prefix: caJsonFormat.prefix,
+    address,
+    port,
+    exposePort: caExposePort,
+    fullAddress: `${address}:${port}`,
+    caAdminNameVar: `${orgName.toUpperCase()}_CA_ADMIN_NAME`,
+    caAdminPassVar: `${orgName.toUpperCase()}_CA_ADMIN_PASSWORD`,
+  };
+}
+
+function transformRootOrgConfig(rootOrgJsonFormat) {
+  const { domain } = rootOrgJsonFormat.organization;
+  const orderersExtended = transformOrderersConfig(
+    rootOrgJsonFormat.orderer,
+    domain,
+  );
+  const ordererHead = orderersExtended[0];
+  return {
+    name: rootOrgJsonFormat.organization.name,
+    mspName: rootOrgJsonFormat.organization.mspName,
+    domain,
+    organization: rootOrgJsonFormat.organization,
+    ca: transformCaConfig(rootOrgJsonFormat.ca, rootOrgJsonFormat.organization.name, domain, 7030),
+    orderers: orderersExtended,
+    ordererHead,
+  };
+}
+
+function extendPeers(peerJsonFormat, domainJsonFormat, headPeerPort, headPeerCouchDbExposePort) {
+  let { anchorPeerInstances } = peerJsonFormat;
+  if (typeof anchorPeerInstances === 'undefined' || anchorPeerInstances === null) {
+    anchorPeerInstances = 1;
+  }
+  return Array(peerJsonFormat.instances)
+    .fill()
+    .map((x, i) => i)
+    .map((i) => {
+      const address = `peer${i}.${domainJsonFormat}`;
+      const port = headPeerPort + i;
+      return {
+        name: `peer${i}`,
+        address,
+        db: peerJsonFormat.db,
+        isAnchorPeer: i < anchorPeerInstances,
+        port,
+        fullAddress: `${address}:${port}`,
+        couchDbExposePort: headPeerCouchDbExposePort + i,
+      };
+    });
+}
+
+function transformOrgConfig(orgJsonFormat, orgNumber) {
+  const orgsCryptoConfigFileName = `crypto-config-${orgJsonFormat.organization.name.toLowerCase()}`;
+  const headPeerPort = 7060 + 10 * orgNumber;
+  const headPeerCouchDbExposePort = 5080 + 10 * orgNumber;
+  const caExposePort = 7031 + orgNumber;
+  const orgName = orgJsonFormat.organization.name;
+  const orgDomain = orgJsonFormat.organization.domain;
+
+  const peersExtended = extendPeers(
+    orgJsonFormat.peer,
+    orgDomain,
+    headPeerPort,
+    headPeerCouchDbExposePort,
+  );
+  const anchorPeers = peersExtended.filter((p) => p.isAnchorPeer);
+  const bootstrapPeersList = anchorPeers.map((a) => a.fullAddress);
+
+  return {
+    key: orgJsonFormat.organization.key,
+    name: orgName,
+    mspName: orgJsonFormat.organization.mspName,
+    domain: orgDomain,
+    cryptoConfigFileName: orgsCryptoConfigFileName,
+    peersCount: peersExtended.length,
+    peers: peersExtended,
+    anchorPeers,
+    bootstrapPeers: bootstrapPeersList.join(' '),
+    ca: transformCaConfig(orgJsonFormat.ca, orgName, orgDomain, caExposePort),
+    cli: {
+      address: `cli.${orgJsonFormat.organization.domain}`,
+    },
+    headPeer: peersExtended[0],
+  };
+}
+
+function transformOrgConfigs(orgsJsonConfigFormat) {
+  return orgsJsonConfigFormat.map((org, currentIndex) => transformOrgConfig(org, currentIndex));
+}
+
+function filterToAvailablePeers(orgTransformedFormat, peersTransformedFormat) {
+  const filteredPeers = orgTransformedFormat.peers.filter(
+    (p) => peersTransformedFormat.includes(p.name),
+  );
+  return {
+    ...orgTransformedFormat,
+    peers: filteredPeers,
+    headPeer: filteredPeers[0],
+  };
+}
+
+function transformChannelConfig(channelJsonFormat, orgsTransformed) {
+  const channelName = channelJsonFormat.name;
+  const profileName = _.chain(channelName).camelCase().upperFirst().value();
+
+  const orgKeys = channelJsonFormat.orgs.map((o) => o.key);
+  const orgPeers = channelJsonFormat.orgs.map((o) => o.peers)
+    .reduce((a, b) => a.concat(b), []);
+  const orgsForChannel = orgsTransformed
+    .filter((org) => orgKeys.includes(org.key))
+    .map((org) => filterToAvailablePeers(org, orgPeers));
+
+  return {
+    key: channelJsonFormat.key,
+    name: channelName,
+    orgs: orgsForChannel,
+    profile: {
+      name: profileName,
+    },
+    instantiatingOrg: orgsForChannel[0],
+  };
+}
+
+function transformChannelConfigs(channelsJsonConfigFormat, orgsTransformed) {
+  return channelsJsonConfigFormat.map((ch) => transformChannelConfig(ch, orgsTransformed));
+}
+
+// Used https://github.com/hyperledger/fabric/blob/v1.4.8/sampleconfig/configtx.yaml for values
+const networkCapabilities = {
+  '2.2.1': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '2.2.0': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '2.1.1': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '2.1.0': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '2.0.1': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+
+  '1.4.8': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '1.4.7': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '1.4.6': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '1.4.5': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '1.4.4': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '1.4.3': { channel: 'V1_4_3', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '1.4.2': { channel: 'V1_4_2', orderer: 'V1_4_2', application: 'V1_4_2' },
+  '1.4.1': { channel: 'V1_3', orderer: 'V1_1', application: 'V1_3' },
+  '1.4.0': { channel: 'V1_3', orderer: 'V1_1', application: 'V1_3' },
+  '1.3.0': { channel: 'V1_3', orderer: 'V1_1', application: 'V1_3' },
+};
+
+function getNetworkCapabilities(fabricVersion) {
+  return networkCapabilities[fabricVersion] || networkCapabilities['1.4.8'];
+}
+
+function isHlf20(fabricVersion) {
+  const supported20Versions = ['2.2.1', '2.2.0', '2.1.1', '2.1.0', '2.0.1'];
+  return supported20Versions.includes(fabricVersion);
+}
+
+function getCaVersion(fabricVersion) {
+  return (version(fabricVersion).isGreaterOrEqual('1.4.10') ? '1.5.0' : fabricVersion);
+}
+
+function getEnvVarOrThrow(name) {
+  const value = process.env[name];
+  if (!value || !value.length) throw new Error(`Missing environment variable ${name}`);
+  return value;
+}
+
+function getPathsFromEnv() {
+  return {
+    fabricaConfig: getEnvVarOrThrow('FABRICA_CONFIG'),
+    chaincodesBaseDir: getEnvVarOrThrow('CHAINCODES_BASE_DIR'),
+  };
+}
+
+function transformNetworkSettings(networkSettingsJson) {
+  return {
+    ...networkSettingsJson,
+    fabricCaVersion: getCaVersion(networkSettingsJson.fabricVersion),
+    paths: getPathsFromEnv(),
+    isHlf20: isHlf20(networkSettingsJson.fabricVersion),
+  };
+}
+
+module.exports = {
+  transformChaincodesConfig,
+  transformRootOrgConfig,
+  transformOrgConfigs,
+  transformChannelConfigs,
+  getNetworkCapabilities,
+  transformNetworkSettings,
+};
--- generators/setup-docker/templates/fabric-docker/channel-query-scripts.sh
+++ generators/setup-docker/templates/fabric-docker/channel-query-scripts.sh
@@ -8,85 +8,84 @@ function channelQuery() {
 
 if [ "$#" -eq 1 ]; then
   printChannelsHelp
-<% orgs.forEach(function(org) { %>
-elif [ "$1" = "list" ] && [ "$2" = "<%= org.mspName.toLowerCase(); %>" ] && [ -n "$3" ]; then
-  PEER_NAME=$3
+<% orgs.forEach(function(org) { org.peers.forEach(function(peer) { %>
+elif [ "$1" = "list" ] && [ "$2" = "<%= org.name.toLowerCase(); %>" ] && [ "$3" = "<%= peer.name %>" ]; then
   <% if(!networkSettings.tls) { -%>
-    peerChannelList "<%= org.cliAddress %>" "${PEER_NAME}.<%= org.domain %>:7051"
+    peerChannelList "<%= org.cli.address %>" "<%= peer.fullAddress %>"
   <% } else { -%>
-    peerChannelListTls "<%= org.cliAddress %>" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelListTls "<%= org.cli.address %>" "$<%= peer.fullAddress %>" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
-<% }) %>
+<% })}) %>
 
 <% channels.forEach(function(channel) { channel.orgs.forEach(function(org) { %>
 elif [ "$1" = "getinfo" ] && [ "$2" = "<%= channel.name %>" ] && [ "$3" = "<%= org.mspName.toLowerCase(); %>" ] && [ -n "$4" ]; then
   PEER_NAME=$4
   <% if(!networkSettings.tls) { -%>
-    peerChannelGetInfo "<%= channel.name %>" "cli.<%= org.domain %>" "${PEER_NAME}.<%= org.domain %>:7051"
+    peerChannelGetInfo "<%= channel.name %>" "<%= org.cli.address %>" "${PEER_NAME}.<%= org.domain %>:7051"
   <% } else { -%>
-    peerChannelGetInfoTls "<%= channel.name %>" "cli.<%= org.domain %>" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelGetInfoTls "<%= channel.name %>" "<%= org.cli.address %>" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
 elif [ "$1" = "fetch" ] && [ "$2" = "config" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 5 ]; then
   FILE_NAME=$5
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchConfigDefault "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}"
+    peerChannelFetchConfigDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}"
   <% } else { -%>
-    peerChannelFetchConfigDefaultTls "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchConfigDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
 elif [ "$1" = "fetch" ] && [ "$2" = "config" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 6 ]; then
   FILE_NAME=$6
   PEER_NAME=$7
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchConfig "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051"
+    peerChannelFetchConfig "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051"
   <% } else { -%>
-    peerChannelFetchConfigTls "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchConfigTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
 elif [ "$1" = "fetch" ] && [ "$2" = "lastBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 5 ]; then
   FILE_NAME=$6
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchLastBlockDefault "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}"
+    peerChannelFetchLastBlockDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}"
   <% } else { -%>
-    peerChannelFetchLastBlockDefaultTls "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchLastBlockDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
 elif [ "$1" = "fetch" ] && [ "$2" = "lastBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 6 ]; then
   FILE_NAME=$6
   PEER_NAME=$7
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchLastBlock "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051"
+    peerChannelFetchLastBlock "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051"
   <% } else { -%>
-    peerChannelFetchLastBlockTls "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchLastBlockTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
 elif [ "$1" = "fetch" ] && [ "$2" = "firstBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 5 ]; then
   FILE_NAME=$6
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchFirstBlockDefault "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}"
+    peerChannelFetchFirstBlockDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}"
   <% } else { -%>
-    peerChannelFetchFirstBlockDefaultTls "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchFirstBlockDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
 elif [ "$1" = "fetch" ] && [ "$2" = "firstBlock" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 6 ]; then
   FILE_NAME=$6
   PEER_NAME=$7
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchFirstBlockDefault "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051"
+    peerChannelFetchFirstBlockDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051"
   <% } else { -%>
-    peerChannelFetchFirstBlockDefaultTls "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchFirstBlockDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
 elif [ "$1" = "fetch" ] && [ "$2" = "block" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 6 ]; then
   FILE_NAME=$6
   BLOCK_NUMBER=$7
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchBlockDefault "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${BLOCK_NUMBER}"
+    peerChannelFetchBlockDefault "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}"
   <% } else { -%>
-    peerChannelFetchBlockDefaultTls "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchBlockDefaultTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
 elif [ "$1" = "fetch" ] && [ "$2" = "block" ] && [ "$3" = "<%= channel.name %>" ] && [ "$4" = "<%= org.mspName.toLowerCase(); %>" ] && [ "$#" = 7 ]; then
   FILE_NAME=$6
   BLOCK_NUMBER=$7
   PEER_NAME=$8
   <% if(!networkSettings.tls) { -%>
-    peerChannelFetchBlock "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "${PEER_NAME}.<%= org.domain %>:7051"
+    peerChannelFetchBlock "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "${PEER_NAME}.<%= org.domain %>:7051"
   <% } else { -%>
-    peerChannelFetchBlockTls "<%= channel.name %>" "cli.<%= org.domain %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
+    peerChannelFetchBlockTls "<%= channel.name %>" "<%= org.cli.address %>" "${FILE_NAME}" "${BLOCK_NUMBER}" "${PEER_NAME}.<%= org.domain %>:7051" "crypto/daTls/msp/tlscacerts/tlsca.<%= rootOrg.organization.domain %>-cert.pem"
   <% } %>
 <% })}) %>
 else
@@ -98,23 +97,23 @@ fi
 function printChannelsHelp() {
   echo "Channel managment commands:"
   echo ""
-  <% orgs.forEach(function(org) { %>
-    echo "fabrica channel list <%= org.mspName.toLowerCase(); %> <peer0>"
-    echo -e "\t List channels on given peer of <%= org.mspName %>".
+  <% orgs.forEach(function(org) { org.peers.forEach(function(peer) { %>
+    echo "fabrica channel list <%= org.name.toLowerCase(); %> <%= peer.name %>"
+    echo -e "\t List channels on '<%= peer.name %>' of '<%= org.name %>'".
     echo ""
-  <% }) %>
+  <% })}) %>
   <% channels.forEach(function(channel) { channel.orgs.forEach(function(org) { %>
-    echo "fabrica channel getinfo <%= channel.name %> <%= org.mspName.toLowerCase(); %> <peer0>"
-    echo -e "\t Get channel info on given peer of <%= org.mspName %>".
+    echo "fabrica channel getinfo <%= channel.name %> <%= org.name.toLowerCase(); %> <peer0>"
+    echo -e "\t Get channel info on given peer of <%= org.name %>".
     echo ""
-    echo "fabrica channel fetch config <%= channel.name %> <%= org.mspName.toLowerCase(); %> <fileName.json> [peer0]"
-    echo -e "\t Download latest config block to current dir. By default it uses first peer of <%= org.mspName %>".
+    echo "fabrica channel fetch config <%= channel.name %> <%= org.name.toLowerCase(); %> <fileName.json> [peer0]"
+    echo -e "\t Download latest config block to current dir. By default it uses first peer of <%= org.name %>".
     echo ""
-    echo "fabrica channel fetch lastBlock <%= channel.name %> <%= org.mspName.toLowerCase(); %> <fileName.json> [peer0]"
-    echo -e "\t Download last, decrypted block to current dir. By default it uses first peer of <%= org.mspName %>".
+    echo "fabrica channel fetch lastBlock <%= channel.name %> <%= org.name.toLowerCase(); %> <fileName.json> [peer0]"
+    echo -e "\t Download last, decrypted block to current dir. By default it uses first peer of <%= org.name %>".
     echo ""
-    echo "fabrica channel fetch firstBlock <%= channel.name %> <%= org.mspName.toLowerCase(); %> <fileName.json> [peer0]"
-    echo -e "\t Download first, decrypted block to current dir. By default it uses first peer of <%= org.mspName %>".
+    echo "fabrica channel fetch firstBlock <%= channel.name %> <%= org.name.toLowerCase(); %> <fileName.json> [peer0]"
+    echo -e "\t Download first, decrypted block to current dir. By default it uses first peer of <%= org.name %>".
     echo ""
   <% })}) %>
 }
