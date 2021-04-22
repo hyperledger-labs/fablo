58e08ef9e3968b8aab24fef8f67a4e66b66a4156
Jakub Dzikowski <jakub.dzikowski@softwaremill.com>
2021-04-22 13:17:08 +0200

    use separate file for defaults

--- generators/extend-config/configTransformers.js
+++ generators/extend-config/configTransformers.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const { version } = require('../repositoryUtils');
+const defaults = require('./defaults');
 
 function createPrivateCollectionConfig(fabricVersion, channel, name, orgNames) {
   // We need only orgs that can have access to private data
@@ -58,7 +59,7 @@ function transformChaincodesConfig(fabricVersion, chaincodes, transformedChannel
 }
 
 function transformOrderersConfig(ordererJsonConfigFormat, rootDomainJsonConfigFormat) {
-  const type = ordererJsonConfigFormat.type === 'raft' ? 'etcdraft' : ordererJsonConfigFormat.type;
+  const consensus = ordererJsonConfigFormat.type === 'raft' ? 'etcdraft' : ordererJsonConfigFormat.type;
 
   return Array(ordererJsonConfigFormat.instances)
     .fill()
@@ -71,7 +72,7 @@ function transformOrderersConfig(ordererJsonConfigFormat, rootDomainJsonConfigFo
         name,
         domain: rootDomainJsonConfigFormat,
         address,
-        consensus: type,
+        consensus,
         port,
         fullAddress: `${address}:${port}`,
       };
@@ -79,11 +80,11 @@ function transformOrderersConfig(ordererJsonConfigFormat, rootDomainJsonConfigFo
 }
 
 function transformCaConfig(caJsonFormat, orgName, orgDomainJsonFormat, caExposePort) {
-  const { prefix } = caJsonFormat || { prefix: 'ca' };
-  const address = `${prefix}.${orgDomainJsonFormat}`;
+  const ca = caJsonFormat || defaults.ca;
+  const address = `${ca.prefix}.${orgDomainJsonFormat}`;
   const port = 7054;
   return {
-    prefix,
+    prefix: ca.prefix,
     address,
     port,
     exposePort: caExposePort,
@@ -95,11 +96,8 @@ function transformCaConfig(caJsonFormat, orgName, orgDomainJsonFormat, caExposeP
 
 function transformRootOrgConfig(rootOrgJsonFormat) {
   const { domain, name } = rootOrgJsonFormat.organization;
-  const mspName = rootOrgJsonFormat.organization.mspName || `${name}MSP`;
-  const orderersExtended = transformOrderersConfig(
-    rootOrgJsonFormat.orderer,
-    domain,
-  );
+  const mspName = rootOrgJsonFormat.organization.mspName || defaults.organization.mspName(name);
+  const orderersExtended = transformOrderersConfig(rootOrgJsonFormat.orderer, domain);
   const ordererHead = orderersExtended[0];
   return {
     name,
@@ -112,20 +110,20 @@ function transformRootOrgConfig(rootOrgJsonFormat) {
 }
 
 function extendPeers(peerJsonFormat, domainJsonFormat, headPeerPort, headPeerCouchDbExposePort) {
-  let { anchorPeerInstances } = peerJsonFormat;
-  if (typeof anchorPeerInstances === 'undefined' || anchorPeerInstances === null) {
-    anchorPeerInstances = 1;
-  }
+  const peerPrefix = peerJsonFormat.prefix || defaults.peer.prefix;
+  const db = peerJsonFormat.db || defaults.peer.prefix;
+  const anchorPeerInstances = peerJsonFormat.anchorPeerInstances
+    || defaults.peer.anchorPeerInstances;
+
   return Array(peerJsonFormat.instances)
     .fill()
-    .map((x, i) => i)
-    .map((i) => {
-      const address = `peer${i}.${domainJsonFormat}`;
+    .map((_x, i) => {
+      const address = `${peerPrefix}${i}.${domainJsonFormat}`;
       const port = headPeerPort + i;
       return {
-        name: `peer${i}`,
+        name: `${peerPrefix}${i}`,
         address,
-        db: peerJsonFormat.db || 'LevelDb',
+        db,
         isAnchorPeer: i < anchorPeerInstances,
         port,
         fullAddress: `${address}:${port}`,
@@ -135,20 +133,14 @@ function extendPeers(peerJsonFormat, domainJsonFormat, headPeerPort, headPeerCou
 }
 
 function transformOrgConfig(orgJsonFormat, orgNumber) {
-  const orgsCryptoConfigFileName = `crypto-config-${orgJsonFormat.organization.name.toLowerCase()}`;
+  const cryptoConfigFileName = `crypto-config-${orgJsonFormat.organization.name.toLowerCase()}`;
   const headPeerPort = 7060 + 10 * orgNumber;
-  const headPeerCouchDbExposePort = 5080 + 10 * orgNumber;
-  const caExposePort = 7031 + orgNumber;
+  const headPeerCouchDbPort = 5080 + 10 * orgNumber;
+  const caPort = 7031 + orgNumber;
   const { domain, name } = orgJsonFormat.organization;
-  const mspName = orgJsonFormat.organization.mspName || `${name}MSP`;
-
-  const peersExtended = extendPeers(
-    orgJsonFormat.peer,
-    domain,
-    headPeerPort,
-    headPeerCouchDbExposePort,
-  );
-  const anchorPeers = peersExtended.filter((p) => p.isAnchorPeer);
+  const mspName = orgJsonFormat.organization.mspName || defaults.organization.mspName(name);
+  const peers = extendPeers(orgJsonFormat.peer, domain, headPeerPort, headPeerCouchDbPort);
+  const anchorPeers = peers.filter((p) => p.isAnchorPeer);
   const bootstrapPeersList = anchorPeers.map((a) => a.fullAddress);
   const bootstrapPeersStringParam = bootstrapPeersList.length === 1
     ? bootstrapPeersList[0] // note no quotes in parameter
@@ -158,16 +150,16 @@ function transformOrgConfig(orgJsonFormat, orgNumber) {
     name,
     mspName,
     domain,
-    cryptoConfigFileName: orgsCryptoConfigFileName,
-    peersCount: peersExtended.length,
-    peers: peersExtended,
+    cryptoConfigFileName,
+    peersCount: peers.length,
+    peers,
     anchorPeers,
     bootstrapPeers: bootstrapPeersStringParam,
-    ca: transformCaConfig(orgJsonFormat.ca, name, domain, caExposePort),
+    ca: transformCaConfig(orgJsonFormat.ca, name, domain, caPort),
     cli: {
       address: `cli.${orgJsonFormat.organization.domain}`,
     },
-    headPeer: peersExtended[0],
+    headPeer: peers[0],
   };
 }
 
@@ -258,11 +250,17 @@ function getPathsFromEnv() {
 }
 
 function transformNetworkSettings(networkSettingsJson) {
+  const monitoring = {
+    ...defaults.networkSettings.monitoring,
+    ...networkSettingsJson.monitoring,
+  };
+
   return {
     ...networkSettingsJson,
     fabricCaVersion: getCaVersion(networkSettingsJson.fabricVersion),
     paths: getPathsFromEnv(),
     isHlf20: isHlf20(networkSettingsJson.fabricVersion),
+    monitoring,
   };
 }
 
