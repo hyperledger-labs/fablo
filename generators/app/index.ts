e5b01adddc47854fcde3bd4c1bb471556338d636
Jakub Dzikowski <jakub.dzikowski@softwaremill.com>
2021-04-24 10:14:22 +0200

    started working on typescript support

---
+++ generators/app/index.ts
@@ -0,0 +1,15 @@
+import Generator from "yeoman-generator";
+import config from "../config";
+
+module.exports = class extends Generator {
+  async initializing() {
+    this.log(config.splashScreen());
+  }
+
+  async displayInfo() {
+    const url = "https://github.com/softwaremill/fabrica";
+    this.log("This is main entry point for Yeoman app used in Fabrica.");
+    this.log("Visit the project page to get more information.");
+    this.log(`---\n${url}\n---`);
+  }
+};
