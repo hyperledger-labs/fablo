5c9b657f4417f1221b0dffe76e77d025b60a9ce5
Jakub Dzikowski <jakub.dzikowski@softwaremill.com>
2021-05-08 11:46:21 +0200

    added chaincode for fabric 2.x
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

---
+++ samples/chaincodes/chaincode-kv-node/index.js
@@ -0,0 +1,50 @@
+const { Contract } = require("fabric-contract-api");
+const crypto = require("crypto");
+
+class KVContract extends Contract {
+  constructor() {
+    super("KVContract");
+  }
+
+  async instantiate() {
+    // function that will be invoked on chaincode instantiation
+  }
+
+  async put(ctx, key, value) {
+    await ctx.stub.putState(key, Buffer.from(value));
+    return { success: "OK" };
+  }
+
+  async get(ctx, key) {
+    const buffer = await ctx.stub.getState(key);
+    if (!buffer || !buffer.length) return { error: "NOT_FOUND" };
+    return { success: buffer.toString() };
+  }
+
+  async putPrivateMessage(ctx, collection) {
+    const transient = ctx.stub.getTransient();
+    const message = transient.get("message");
+    await ctx.stub.putPrivateData(collection, "message", message);
+    return { success: "OK" };
+  }
+
+  async getPrivateMessage(ctx, collection) {
+    const message = await ctx.stub.getPrivateData(collection, "message");
+    const messageString = message.toBuffer ? message.toBuffer().toString() : message.toString();
+    return { success: messageString };
+  }
+
+  async verifyPrivateMessage(ctx, collection) {
+    const transient = ctx.stub.getTransient();
+    const message = transient.get("message");
+    const messageString = message.toBuffer ? message.toBuffer().toString() : message.toString();
+    const currentHash = crypto.createHash("sha256").update(messageString).digest("hex");
+    const privateDataHash = (await ctx.stub.getPrivateDataHash(collection, "message")).toString("hex");
+    if (privateDataHash !== currentHash) {
+      return { error: "VERIFICATION_FAILED" };
+    }
+    return { success: "OK" };
+  }
+}
+
+exports.contracts = [KVContract];
---
+++ samples/chaincodes/chaincode-kv-node/index.spec.js
@@ -0,0 +1,101 @@
+const { JSONSerializer } = require("fabric-contract-api");
+const { ChaincodeMockStub } = require("@theledger/fabric-mock-stub");
+const ChaincodeFromContract = require("fabric-shim/lib/contract-spi/chaincodefromcontract");
+const uuid = require("uuid");
+const { contracts } = require("./index");
+
+const [KVContract] = contracts;
+
+const getChaincodeForContract = (contract) => {
+  const serializers = {
+    serializers: {
+      jsonSerializer: JSONSerializer,
+    },
+    transaction: "jsonSerializer",
+  };
+  return new ChaincodeFromContract([contract], serializers, { info: {} });
+};
+
+class KVContractMockStub extends ChaincodeMockStub {
+  constructor() {
+    super(KVContract.toString(), getChaincodeForContract(KVContract));
+  }
+
+  getBufferArgs() {
+    return this.getArgs().map((x) => Buffer.from(x));
+  }
+
+  async mockInvokeJson(args, transient) {
+    const transientMap = !transient
+      ? undefined
+      : Object.keys(transient).reduce((map, k) => map.set(k, transient[k]), new Map());
+    const response = await this.mockInvoke(uuid.v1(), args, transientMap);
+
+    if (response.status !== 200) {
+      console.error(`Error invoking chaincode. Status: ${response.status}, message:`, response.message);
+      throw response.message;
+    }
+
+    return JSON.parse(response.payload.toString());
+  }
+}
+
+describe("KVContract", () => {
+  it("should put and get value", async () => {
+    // Given
+    const stub = new KVContractMockStub();
+    const key = "ship";
+    const value = "Black Pearl";
+
+    // When
+    const putResponse = await stub.mockInvokeJson(["put", key, value]);
+    const getResponse = await stub.mockInvokeJson(["get", key]);
+
+    // Then
+    expect(putResponse).toEqual({ success: "OK" });
+    expect(getResponse).toEqual({ success: "Black Pearl" });
+  });
+
+  it("should return missing value error", async () => {
+    // Given
+    const stub = new KVContractMockStub();
+    const key = "ship";
+
+    // When
+    const getResponse = await stub.mockInvokeJson(["get", key]);
+
+    // Then
+    expect(getResponse).toEqual({ error: "NOT_FOUND" });
+  });
+
+  it("should put private data", async () => {
+    // Given
+    const stub = new KVContractMockStub();
+    const key = "diary";
+    const message = "Black Pearl was initially owned by Jack Sparrow";
+
+    // When
+    const response = await stub.mockInvokeJson(["putPrivateMessage", key], { message });
+
+    // Then
+    expect(response).toEqual({ success: "OK" });
+  });
+
+  // Just for the reference. Method 'getPrivateDataHash' is not implemented in @theledger/fabric-mock-stub lib
+  it.skip("should verify private data", async () => {
+    // Given
+    const stub = new KVContractMockStub();
+    const key = "diary";
+    const message = { message: "Black Pearl was initially owned by Jack Sparrow" };
+    const wrongMessage = { message: "Owned by Willy Wonka" };
+    await stub.mockInvokeJson(["putPrivateMessage", key], message);
+
+    // When
+    const validResponse = await stub.mockInvokeJson(["verifyPrivateMessage", key], message);
+    const invalidResponse = await stub.mockInvokeJson(["verifyPrivateMessage", key], wrongMessage);
+
+    // Then
+    expect(validResponse).toEqual({ success: "OK" });
+    expect(invalidResponse).toEqual({ success: "OK" });
+  });
+});
