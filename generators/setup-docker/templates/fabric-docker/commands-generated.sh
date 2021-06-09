7d2b8d675d2b780fb9296d14a09bc6366c691453
Jakub Dzikowski <jakub.dzikowski@softwaremill.com>
2021-06-09 09:26:41 +0200

    package chaincodes to a different dir
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- generators/setup-docker/templates/fabric-docker/commands-generated.sh
+++ generators/setup-docker/templates/fabric-docker/commands-generated.sh
@@ -181,6 +181,7 @@ function networkDown() {
   printf "\nRemoving generated configs... \U1F5D1 \n"
   rm -rf "$FABRICA_NETWORK_ROOT"/fabric-config/config
   rm -rf "$FABRICA_NETWORK_ROOT"/fabric-config/crypto-config
+  rm -rf "$FABRICA_NETWORK_ROOT"/fabric-config/chaincode-packages
 
   printHeadline "Done! Network was purged" "U1F5D1"
 }
