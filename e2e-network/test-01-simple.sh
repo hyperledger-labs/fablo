f8a3082103e58dbc66a4c897186bcbe019cded52
Piotr Hejwowski <piotr.hejwowski@gmail.com>
2021-10-27 16:07:12 +0200

    Orderer sharding, part 2 - updated tests
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- e2e-network/test-01-simple.sh
+++ e2e-network/test-01-simple.sh
@@ -42,7 +42,7 @@ trap 'networkDown ; echo "Test failed" ; exit 1' ERR SIGINT
 # start the network
 networkUp
 
-waitForContainer "orderer0.orderer.com" "Created and started new.*my-channel1"
+waitForContainer "orderer0.group1.orderer.com" "Created and started new.*my-channel1"
 waitForContainer "ca.org1.com" "Listening on http://0.0.0.0:7054"
 waitForContainer "peer0.org1.com" "Joining gossip network of channel my-channel1 with 1 organizations"
 waitForContainer "peer1.org1.com" "Joining gossip network of channel my-channel1 with 1 organizations"
