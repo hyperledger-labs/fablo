25b31d19d9ba82bbd3527feb379b8ad2453b0b21
Jakub Dzikowski <jakub.dzikowski@softwaremill.com>
2021-11-01 09:03:53 +0100

    minor changes after merge

--- src/extend-config/extendConfig.ts
+++ src/extend-config/extendConfig.ts
@@ -1,32 +1,11 @@
-import * as _ from "lodash";
 import { FabloConfigJson } from "../types/FabloConfigJson";
-import { FabloConfigExtended, OrdererGroup, OrgConfig } from "../types/FabloConfigExtended";
+import { FabloConfigExtended } from "../types/FabloConfigExtended";
 import { extendOrgsConfig } from "./extendOrgsConfig";
 import extendNetworkSettings from "./extendNetworkSettings";
 import extendChannelsConfig from "./extendChannelsConfig";
 import extendChaincodesConfig from "./extendChaincodesConfig";
 import extendHooksConfig from "./extendHooksConfig";
-
-// TODO
-const mergeOrdererGroupsByName = (orgs: OrgConfig[]): OrdererGroup[] => {
-  const ordererGroups = orgs.flatMap((o) => o.ordererGroups);
-
-  const ordererGroupsGrouped: Record<string, OrdererGroup[]> = _.groupBy(ordererGroups, (group) => group.name);
-
-  return Object.values(ordererGroupsGrouped).flatMap((groupsWithSameGroupName) => {
-    const orderers = groupsWithSameGroupName.flatMap((group) => group.orderers);
-    const hostingOrgs = groupsWithSameGroupName.flatMap((group) => group.hostingOrgs);
-    const ordererHeads = groupsWithSameGroupName.flatMap((group) => group.ordererHeads);
-
-    return {
-      ...groupsWithSameGroupName[0],
-      hostingOrgs,
-      orderers,
-      ordererHeads,
-      ordererHead: ordererHeads[0],
-    };
-  });
-};
+import { mergeOrdererGroups } from "./mergeOrdererGroups";
 
 const extendConfig = (json: FabloConfigJson): FabloConfigExtended => {
   const {
@@ -39,7 +18,7 @@ const extendConfig = (json: FabloConfigJson): FabloConfigExtended => {
 
   const networkSettings = extendNetworkSettings(networkSettingsJson);
   const orgs = extendOrgsConfig(orgsJson, networkSettings);
-  const ordererGroups = mergeOrdererGroupsByName(orgs);
+  const ordererGroups = mergeOrdererGroups(orgs);
 
   const channels = extendChannelsConfig(channelsJson, orgs, ordererGroups);
   const chaincodes = extendChaincodesConfig(chaincodesJson, channels, networkSettings);
---
+++ src/extend-config/mergeOrdererGroups.ts
@@ -0,0 +1,22 @@
+import { OrdererGroup, OrgConfig } from "../types/FabloConfigExtended";
+import * as _ from "lodash";
+
+export const mergeOrdererGroups = (orgs: OrgConfig[]): OrdererGroup[] => {
+  const ordererGroups = orgs.flatMap((o) => o.ordererGroups);
+
+  const ordererGroupsGrouped: Record<string, OrdererGroup[]> = _.groupBy(ordererGroups, (group) => group.name);
+
+  return Object.values(ordererGroupsGrouped).flatMap((groupsWithSameGroupName) => {
+    const orderers = groupsWithSameGroupName.flatMap((group) => group.orderers);
+    const hostingOrgs = groupsWithSameGroupName.flatMap((group) => group.hostingOrgs);
+    const ordererHeads = groupsWithSameGroupName.flatMap((group) => group.ordererHeads);
+
+    return {
+      ...groupsWithSameGroupName[0],
+      hostingOrgs,
+      orderers,
+      ordererHeads,
+      ordererHead: ordererHeads[0],
+    };
+  });
+};
