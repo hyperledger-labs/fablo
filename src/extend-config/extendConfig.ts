350a49abecbb2a1ff407a38eb633662b80b76df6
Piotr Hejwowski <piotr.hejwowski@gmail.com>
2021-11-06 19:39:20 +0100

    Post generate hook - fixes after merge
    Signed-off-by: Piotr Hejwowski <piotr.hejwowski@gmail.com>

--- src/extend-config/extendConfig.ts
+++ src/extend-config/extendConfig.ts
@@ -1,30 +1,11 @@
 import { FabloConfigJson } from "../types/FabloConfigJson";
-import { FabloConfigExtended, OrdererGroup, OrgConfig } from "../types/FabloConfigExtended";
+import { FabloConfigExtended } from "../types/FabloConfigExtended";
 import { extendOrgsConfig } from "./extendOrgsConfig";
 import extendNetworkSettings from "./extendNetworkSettings";
 import extendChannelsConfig from "./extendChannelsConfig";
 import extendChaincodesConfig from "./extendChaincodesConfig";
-import _ = require("lodash");
-
-const mergeOrdererGroupsByName = (orgs: OrgConfig[]): OrdererGroup[] => {
-  const ordererGroups = orgs.flatMap((o) => o.ordererGroups);
-
-  const ordererGroupsGrouped: Record<string, OrdererGroup[]> = _.groupBy(ordererGroups, (group) => group.name);
-
-  return Object.values(ordererGroupsGrouped).flatMap((groupParts) => {
-    const orderers = groupParts.flatMap((group) => group.orderers);
-    const hostingOrgs = groupParts.flatMap((group) => group.hostingOrgs);
-    const ordererHeads = groupParts.flatMap((group) => group.ordererHeads);
-
-    return {
-      ...groupParts[0],
-      hostingOrgs,
-      orderers,
-      ordererHeads,
-      ordererHead: ordererHeads[0],
-    };
-  });
-};
+import extendHooksConfig from "./extendHooksConfig";
+import { mergeOrdererGroups } from "./mergeOrdererGroups";
 
 const extendConfig = (json: FabloConfigJson): FabloConfigExtended => {
   const {
@@ -32,14 +13,16 @@ const extendConfig = (json: FabloConfigJson): FabloConfigExtended => {
     orgs: orgsJson,
     channels: channelsJson,
     chaincodes: chaincodesJson,
+    hooks: hooksJson,
   } = json;
 
   const networkSettings = extendNetworkSettings(networkSettingsJson);
   const orgs = extendOrgsConfig(orgsJson, networkSettings);
-  const ordererGroups = mergeOrdererGroupsByName(orgs);
+  const ordererGroups = mergeOrdererGroups(orgs);
 
   const channels = extendChannelsConfig(channelsJson, orgs, ordererGroups);
   const chaincodes = extendChaincodesConfig(chaincodesJson, channels, networkSettings);
+  const hooks = extendHooksConfig(hooksJson);
 
   return {
     networkSettings,
@@ -47,6 +30,7 @@ const extendConfig = (json: FabloConfigJson): FabloConfigExtended => {
     orgs,
     channels,
     chaincodes,
+    hooks,
   };
 };
 
