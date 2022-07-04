2d49da8ba3c742f15531442d3b2025373b80f47e
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2022-07-04 16:35:05 +0200

    Update Fablo version to 1.1.0
    Signed-off-by: Jakub Dzikowski <jakub.t.dzikowski@gmail.com>

--- fablo.sh
+++ fablo.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-FABLO_VERSION="1.1.0-unstable"
-FABLO_IMAGE_NAME="softwaremill/fablo"
+FABLO_VERSION="1.1.0"
+FABLO_IMAGE_NAME="fabloio/fablo"
 FABLO_IMAGE="$FABLO_IMAGE_NAME:$FABLO_VERSION"
 
 COMMAND="$1"
--- src/config.test.ts
+++ src/config.test.ts
@@ -1,7 +1,7 @@
 import { fabloVersion, getVersionFromSchemaUrl } from "./config";
 
 it("should get version from schema URL", () => {
-  const url = "https://github.com/softwaremill/fablo/releases/download/0.0.1/schema.json";
+  const url = "https://github.com/hyperledger-labs/fablo/releases/download/0.0.1/schema.json";
   const version = getVersionFromSchemaUrl(url);
   expect(version).toEqual("0.0.1");
 });
