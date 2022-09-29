1e4f49ea9aab1cdf1907cccb826ff462e15ec4b0
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2022-09-29 07:08:47 -0500

    fix: raft, private-data, snapshot docker tests path
    Signed-off-by: Umegbewe Nwebedu <nwebedujunior55@gmail.com>

--- e2e-network/docker/test-02-raft-2orgs.sh
+++ e2e-network/docker/test-02-raft-2orgs.sh
@@ -4,7 +4,7 @@ set -e
 
 TEST_TMP="$(rm -rf "$0.tmpdir" && mkdir -p "$0.tmpdir" && (cd "$0.tmpdir" && pwd))"
 TEST_LOGS="$(mkdir -p "$0.logs" && (cd "$0.logs" && pwd))"
-FABLO_HOME="$TEST_TMP/../.."
+FABLO_HOME="$TEST_TMP/../../.."
 
 CONFIG="$FABLO_HOME/samples/fablo-config-hlf2-2orgs-2chaincodes-raft.yaml"
 
