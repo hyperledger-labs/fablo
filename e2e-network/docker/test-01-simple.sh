416a420b9f2d1dea9618e7cc67c00f34d3a0fd85
Great <nwebedujunior55@gmail.com>
2022-10-26 17:22:53 -0500

    unbound variable fix?
    Signed-off-by: Great <nwebedujunior55@gmail.com>

--- e2e-network/docker/test-01-simple.sh
+++ e2e-network/docker/test-01-simple.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+# set -e
 
 TEST_TMP="$(rm -rf "$0.tmpdir" && mkdir -p "$0.tmpdir" && (cd "$0.tmpdir" && pwd))"
 TEST_LOGS="$(mkdir -p "$0.logs" && (cd "$0.logs" && pwd))"
