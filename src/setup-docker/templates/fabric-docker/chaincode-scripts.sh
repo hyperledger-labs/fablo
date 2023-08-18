13b3f5f04508c6f9562a45339572a86f18c36ac1
jnjerin <njerijoan24@gmail.com>
2023-08-18 10:39:56 +0300

    standardized variable naming convention
    Signed-off-by: jnjerin <njerijoan24@gmail.com>

--- src/setup-docker/templates/fabric-docker/chaincode-scripts.sh
+++ src/setup-docker/templates/fabric-docker/chaincode-scripts.sh
@@ -2,52 +2,50 @@
 
 # Function to perform chaincode invoke
 chaincodeInvoke() {
-  local channel="$1"
-  local chaincode="$2"
-  local peers="$3"
-  local command="$4"
-  local transient="$5"
+  local CHANNEL="$1"
+  local CHAINCODE="$2"
+  local PEERS="$3"
+  local COMMAND="$4"
+  local TRANSIENT="$5"
 
   echo "-> Chaincode invoke:"
-  echo "Channel: $channel"
-  echo "Chaincode: $chaincode"
-  echo "Peers: $peers"
-  echo "Command: $command"
-  echo "Transient: $transient"
+  echo "CHANNEL: $CHANNEL"
+  echo "CHAINCODE: $CHAINCODE"
+  echo "PEERS: $PEERS"
+  echo "COMMAND: $COMMAND"
+  echo "TRANSIENT: $TRANSIENT"
 
 # Validate the input parameters
-  if [[ -z $channel || -z $chaincode || -z $peers || -z $command || -z $transient ]]; then
+  if [[ -z $CHANNEL || -z $CHAINCODE || -z $PEERS || -z $COMMAND || -z $TRANSIENT ]]; then
     echo "Error: Insufficient arguments provided."
     echo "Usage: fablo chaincode invoke <channel_name> <chaincode_name> <peers_domains_comma_separated> <command> <transient>"
     return 1
   fi
 
 
-  peerAddresses="--peerAddresses $(echo "$peers" | sed 's/,/ --peerAddresses /g')"
+ PEER_ADDRESSES="--peerAddresses $(echo "$PEERS" | sed 's/,/ --peerAddresses  /g')"
 
-cli=""
+CLI=""
 
 <% orgs.forEach((org) => { -%>
   <% org.peers.forEach((peer) => { -%>
-    if [ "$peers" = "<%= peer.fullAddress %>" ]; then
-      cli="<%= org.cli.address %>"
+    if [ "$PEERS" = "<%= peer.fullAddress %>" ]; then
+      CLI="<%= org.cli.address %>"
     fi
   <% }) -%>
 <% }) -%>
-if [ -z "$cli" ]; then
-  echo "Unknown peer: $peers"
+if [ -z "$CLI" ]; then
+  echo "Unknown peer: $PEERS"
   exit 1
 fi
 
-  docker exec "$cli"  peer chaincode invoke \
-    $peerAddresses \
-    -C "$channel" \
-    -n "$chaincode" \
-    -c "$command" \
-    --transient "$transient" \
+  docker exec "$CLI"  peer chaincode invoke \
+    $PEER_ADDRESSES \
+    -C "$CHANNEL" \
+    -n "$CHAINCODE" \
+    -c "$COMMAND" \
+    --transient "$TRANSIENT" \
     --waitForEvent \
     --waitForEventTimeout 90s \
     2>&1 
-
-  echo "Executing chaincode invoke command..."
 }
