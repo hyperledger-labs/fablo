c7dadc145bdc65907a6475ff9c195310189757f2
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2024-09-17 16:41:58 +0200

    Set Docker registry to ghcr.io/fablo-io/fablo
    Signed-off-by: Jakub Dzikowski <jakub.t.dzikowski@gmail.com>

--- fablo.sh
+++ fablo.sh
@@ -3,7 +3,7 @@
 set -e
 
 FABLO_VERSION=1.2.1-unstable.0
-FABLO_IMAGE_NAME="ghcr.io/hyperledger-labs/fablo"
+FABLO_IMAGE_NAME="ghcr.io/fablo-io/fablo"
 FABLO_IMAGE="$FABLO_IMAGE_NAME:$FABLO_VERSION"
 
 COMMAND="$1"
