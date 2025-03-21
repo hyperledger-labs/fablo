8391e74123f6f0bde3a5d994eb5b492177eb06fc
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2025-03-21 11:30:40 +0100

    remove fabric tools
    Signed-off-by: Umegbewe Nwebedu <nwebedujunior55@gmail.com>

--- fablo-build.sh
+++ fablo-build.sh
@@ -16,79 +16,34 @@ echo "   FABLO_VERSION: $FABLO_VERSION"
 echo "   VERSION_DETAILS: $VERSION_DETAILS"
 
 IMAGE_BASE_NAME="ghcr.io/fablo-io/fablo:$FABLO_VERSION"
-TOOLS_IMAGE_BASE_NAME="ghcr.io/fablo-io/fabric-tools:3.0.0"
 
-build_and_push_tools() {
-    local platform
-    local arch
-    
-    platform=$(uname -s | tr '[:upper:]' '[:lower:]')
-    case $(uname -m) in
-        x86_64)
-            arch="amd64"
-            ;;
-        aarch64|arm64)
-            arch="arm64"
-            ;;
-        *)
-            echo "Unsupported architecture: $(uname -m)"
-            exit 1
-            ;;
-    esac
+if [ "$(command -v nvm)" != "nvm" ] && [ -f ~/.nvm/nvm.sh ]; then
+  set +e
+  # shellcheck disable=SC2039
+  source ~/.nvm/nvm.sh
+  set -e
+fi
+if [ "$(command -v nvm)" = "nvm" ]; then
+  set +u
+  nvm install
+  set -u
+fi
 
-    if [ "${1:-''}" = "--push" ]; then
-        docker buildx build \
-            --build-arg FABRIC_VERSION=3.0.0 \
-            --build-arg ARCH="$arch" \
-            --build-arg PLATFORM="$platform" \
-            --platform linux/amd64,linux/arm64 \
-            --tag "$TOOLS_IMAGE_BASE_NAME" \
-            --push \
-            "$FABLO_HOME/fabric-tools"
-    else
-        docker build \
-            --build-arg FABRIC_VERSION=3.0.0 \
-            --build-arg ARCH="$arch" \
-            --build-arg PLATFORM="$platform" \
-            --tag "$TOOLS_IMAGE_BASE_NAME" \
-            "$FABLO_HOME/fabric-tools"
-    fi
-}
+npm install
+npm run build:dist
 
-build_and_push_fablo() {
-    if [ "$(command -v nvm)" != "nvm" ] && [ -f ~/.nvm/nvm.sh ]; then
-        set +e
-        # shellcheck disable=SC2039
-        source ~/.nvm/nvm.sh
-        set -e
-    fi
-    if [ "$(command -v nvm)" = "nvm" ]; then
-        set +u
-        nvm install
-        set -u
-    fi
-
-    npm install
-    npm run build:dist
-
-    if [ "${1:-''}" = "--push" ]; then
-        docker buildx build \
-            --build-arg VERSION_DETAILS="$VERSION_DETAILS" \
-            --platform linux/amd64,linux/arm64 \
-            --tag "$IMAGE_BASE_NAME" \
-            --push \
-            "$FABLO_HOME"
-    else
-        docker build \
-            --build-arg VERSION_DETAILS="$VERSION_DETAILS" \
-            --tag "$IMAGE_BASE_NAME" "$FABLO_HOME"
-
-        docker tag "$IMAGE_BASE_NAME" "ghcr.io/fablo-io/fablo:$FABLO_VERSION"
-    fi
-}
-
-if [ "${1:-''}" = "tools" ]; then
-    build_and_push_tools "${2:-''}"
+# if --push is passed, then build for all platforms and push the image to the registry
+if [ "${1:-''}" = "--push" ]; then
+  docker buildx build \
+    --build-arg VERSION_DETAILS="$VERSION_DETAILS" \
+    --platform linux/amd64,linux/arm64 \
+    --tag "ghcr.io/fablo-io/fablo:$FABLO_VERSION" \
+    --push \
+    "$FABLO_HOME"
 else
-    build_and_push_fablo "${1:-''}"
+  docker build \
+    --build-arg VERSION_DETAILS="$VERSION_DETAILS" \
+    --tag "$IMAGE_BASE_NAME" "$FABLO_HOME"
+
+  docker tag "$IMAGE_BASE_NAME" "ghcr.io/fablo-io/fablo:$FABLO_VERSION"
 fi
