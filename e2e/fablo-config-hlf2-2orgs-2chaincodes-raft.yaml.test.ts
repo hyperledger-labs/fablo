6c0299e900a65002ee51e9fcf3ea20b1e3e4ddb9
Tushar Saxena <165766280+IITI-tushar@users.noreply.github.com>
2025-04-14 00:34:09 +0530

    Add check for unique chaincode names across channels
    * Add `checkUniqueChaincodeNames` function in `extendChaincodesConfig.ts` to ensure chaincode names are unique and throw an error if a duplicate is found
    * Call `checkUniqueChaincodeNames` in `extendConfig.ts` before extending chaincodes
    * Add test cases in `fablo-config-hlf2-2orgs-2chaincodes-raft.yaml.test.ts` and `fablo-config-hlf2-2orgs-2chaincodes-private-data.yaml.test.ts` to verify that duplicate chaincode names across different channels are not allowed
    
    Signed-off-by: IITI-tushar <019saxenatushar@gmail.com>
    
    :

--- e2e/fablo-config-hlf2-2orgs-2chaincodes-raft.yaml.test.ts
+++ e2e/fablo-config-hlf2-2orgs-2chaincodes-raft.yaml.test.ts
@@ -5,3 +5,11 @@ const config = "samples/fablo-config-hlf2-2orgs-2chaincodes-raft.yaml";
 describe(config, () => {
   performTests(config);
 });
+
+const duplicateChaincodeConfig = "samples/fablo-config-hlf2-2orgs-2chaincodes-raft-duplicate.yaml";
+
+describe(duplicateChaincodeConfig, () => {
+  it("should throw an error for duplicate chaincode names across different channels", () => {
+    expect(() => performTests(duplicateChaincodeConfig)).toThrowError(/Duplicate chaincode name found/);
+  });
+});
