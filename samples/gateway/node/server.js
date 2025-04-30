238d4aa71039a001c6b75d0f9182e19f2e3fcf3c
Ronald L <ronaldlong46@icloud.com>
2025-04-30 10:43:23 -0400

    Fixed indentation
    Signed-off-by: Ronald L <ronaldlong46@icloud.com>

--- samples/gateway/node/server.js
+++ samples/gateway/node/server.js
@@ -12,27 +12,27 @@ async function connection() {
   const privateKey = crypto.createPrivateKey(privateKeyPem);
   const signer = signers.newPrivateKeySigner(privateKey);  
   const tlsRootCert = await fs.readFile(
-      process.env.TLS_ROOT_CERT,
-    );
+    process.env.TLS_ROOT_CERT,
+  );
   const client = new grpc.Client(process.env.PEER_GATEWAY_URL, grpc.credentials.createSsl(tlsRootCert), {
-      "grpc.ssl_target_name_override": process.env.PEER_ORG_NAME,
+    "grpc.ssl_target_name_override": process.env.PEER_ORG_NAME,
   });  
   const gateway = connect({
-      identity: { mspId: process.env.MSP_ID, credentials },
-      signer,
-      hash: hash.sha256,
-      client,
+    identity: { mspId: process.env.MSP_ID, credentials },
+    signer,
+    hash: hash.sha256,
+    client,
   });  
   try {
-      const network = gateway.getNetwork(process.env.CHANNEL_NAME);
-      const contract = network.getContract(process.env.CONTRACT_NAME);  
-      const putResult = await contract.submitTransaction('put', 'time', new Date().toISOString());
-      console.log('Put result:', utf8Decoder.decode(putResult));  
-      const getResult = await contract.evaluateTransaction('get', 'time');
-      console.log('Get result:', utf8Decoder.decode(getResult));
+    const network = gateway.getNetwork(process.env.CHANNEL_NAME);
+    const contract = network.getContract(process.env.CONTRACT_NAME);  
+    const putResult = await contract.submitTransaction('put', 'time', new Date().toISOString());
+    console.log('Put result:', utf8Decoder.decode(putResult));  
+    const getResult = await contract.evaluateTransaction('get', 'time');
+    console.log('Get result:', utf8Decoder.decode(getResult));
   } finally {
-      gateway.close();
-      client.close();
+    gateway.close();
+    client.close();
   }
 }
 
