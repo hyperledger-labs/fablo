38e8be2a1a4134eaea915165d338c9cc9c39f2a3
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2025-07-14 13:26:30 +0200

    PoC (checkpoint working)
    Signed-off-by: Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
    Signed-off-by: Pereowei Daniel <iwenyadaniel12@gmail.com>

--- src/init/index.ts
+++ src/init/index.ts
@@ -55,7 +55,7 @@ function getDefaultFabloConfig(): FabloConfigJson {
         orgs: [
           {
             name: "Org1",
-            peers: ["peer0", "peer1"],
+            peers: ["peer0"],
           },
         ],
       },
