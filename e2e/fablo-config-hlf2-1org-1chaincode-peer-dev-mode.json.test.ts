7237868c2a1e8fe750fcabbc0b1ecab2bae2c6c8
Pereowei Daniel <iwenyadaniel12@gmail.com>
2025-08-21 06:37:06 -0500

    fix-update branch and snapshots
    Signed-off-by: Pereowei Daniel <iwenyadaniel12@gmail.com>

--- e2e/fablo-config-hlf2-1org-1chaincode-peer-dev-mode.json.test.ts
+++ e2e/fablo-config-hlf2-1org-1chaincode-peer-dev-mode.json.test.ts
@@ -1,6 +1,5 @@
 import performTests from "./performTests";
 
-// TODO RENAME
 const config = "samples/fablo-config-hlf2-1org-1chaincode-peer-dev-mode.json";
 
 describe(config, () => {
--- e2e/fablo-config-hlf2-1org-1chaincode-raft-explorer.json.test.ts
+++ e2e/fablo-config-hlf2-1org-1chaincode-raft-explorer.json.test.ts
@@ -1,6 +1,5 @@
 import performTests from "./performTests";
 
-// TODO RENAME
 const config = "samples/fablo-config-hlf2-1org-1chaincode-raft-explorer.json";
 
 describe(config, () => {
