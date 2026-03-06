c3ecfba3bc85bbfd98395cce553c68d577208c86
OsamaRab3 <osrab3@gmail.com>
2026-03-06 23:47:48 +0200

    refactor: remove main entry point class
    Signed-off-by: OsamaRab3 <osrab3@gmail.com>

--- src/app/index.ts
+++
@@ -1,9 +0,0 @@
-
-export default class {
-  displayInfo(): void {
-    const url = "https://github.com/hyperledger-labs/fablo";
-    console.log("This is the main entry point for Fablo.");
-    console.log("Visit the project page to get more information.");
-    console.log(`---\n${url}\n---`);
-  }
-}
