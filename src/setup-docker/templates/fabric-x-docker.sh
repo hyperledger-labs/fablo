ead130ebe7b5b7b8d8d0c8b14b6950d8968a89aa
Maryam Arif <www.maryamarif28@gmail.com>
2026-08-18 12:58:11 +0500

    Add Fabric-X lifecycle commands (#799)
    Signed-off-by: MaryamArif7 <www.maryamarif28@gmail.com>

---
+++ src/setup-docker/templates/fabric-x-docker.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -eu
+
+FABLO_NETWORK_ROOT="$(cd "$(dirname "$0")" && pwd)"
+FABRIC_X_ROOT="$FABLO_NETWORK_ROOT/fabric-x"
+
+source "$FABLO_NETWORK_ROOT/fabric-x/scripts/base-help.sh"
+source "$FABLO_NETWORK_ROOT/fabric-x/scripts/base-functions.sh"
+
+if [ "$1" = "up" ]; then
+  networkUp
+elif [ "$1" = "down" ]; then
+  networkDown
+elif [ "$1" = "reset" ]; then
+  networkDown
+  networkUp
+elif [ "$1" = "start" ]; then
+  startNetwork
+elif [ "$1" = "stop" ]; then
+  stopNetwork
+elif [ "$1" = "namespace" ] && [ "$2" = "list" ]; then
+  namespaceList
+elif [ "$1" = "namespace" ] && [ "$2" = "init" ]; then
+  namespaceInit
+elif [ "$1" = "help" ]; then
+  printHelp
+elif [ "$1" = "--help" ]; then
+  printHelp
+else
+  echo "No command specified"
+  echo "Basic commands are: up, down, start, stop, reset"
+  echo "Also check: 'namespace list', 'namespace init'"
+  echo "Use 'help' or '--help' for more information"
+fi
