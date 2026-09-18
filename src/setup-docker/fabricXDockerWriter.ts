3c4a384a12911dc5169d4f6bff6f45cb21387a5f
Maryam Arif <www.maryamarif28@gmail.com>
2026-09-18 17:50:59 +0500

    feat: Making the Fabric-X channel configurable (#824)
    * fix: Making the Fabric-X channel configurable
    
    Signed-off-by: MaryamArif7 <www.maryamarif28@gmail.com>
    
    * fix:minor backslash fix
    
    Signed-off-by: MaryamArif7 <www.maryamarif28@gmail.com>
    
    * modified channle name in FabricXDockerWriter
    
    Signed-off-by: MaryamArif7 <www.maryamarif28@gmail.com>
    
    * channel config
    
    Signed-off-by: MaryamArif7 <www.maryamarif28@gmail.com>
    ---------
    
    Signed-off-by: MaryamArif7 <www.maryamarif28@gmail.com>

--- src/setup-docker/fabricXDockerWriter.ts
+++ src/setup-docker/fabricXDockerWriter.ts
@@ -3,13 +3,30 @@ import { renderTemplate, getTemplatePath, getDestinationPath } from "../utils/te
 import * as fs from "fs-extra";
 import * as path from "path";
 
+export interface FabricXTemplateModel {
+  channelName: string;
+  channelProfileName: string;
+}
+
+export const getFabricXTemplateModel = (configExtended: FabloConfigExtended): FabricXTemplateModel => {
+  const [channel] = configExtended.channels;
+  if (!channel) {
+    throw new Error("Fabric-X generation requires exactly one channel.");
+  }
+  return {
+    channelName: channel.name,
+    channelProfileName: channel.profileName,
+  };
+};
+
 export class FabricXDockerWriter {
+  
   constructor(private templatesDir: string, private outputDir: string, private log: (msg: string) => void) {}
 
   public async write(configExtended: FabloConfigExtended): Promise<void> {
     this.log("Generating Fabric-X network files...");
-
-    const data = configExtended as unknown as Record<string, unknown>;
+    const fabricX = getFabricXTemplateModel(configExtended);
+    const data = { ...configExtended, fabricX } as unknown as Record<string, unknown>;
 
     await this.renderTemplateFile("fabric-x-docker.sh", data);
     await this.renderTemplateDirectory("fabric-x", data);
